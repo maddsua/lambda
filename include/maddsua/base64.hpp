d61e563d1b68c6192ce3d7b9ef866b95014ee95d
maddsua <maddsuapro@gmail.com>
2023-01-07 22:37:10 +0200

    base64 function changes

--- include/maddsua/base64.hpp
+++ include/maddsua/base64.hpp
@@ -8,9 +8,30 @@
 
 namespace maddsua {
 
-	std::string b64Decode(std::string encoded);
-	std::string b64Encode(std::string source);
-	bool b64Validate(std::string* encoded);
+	void b64Decode(std::string* encoded, std::string* plain);
+	void b64Encode(std::string* plain, std::string* encoded);
+
+	inline std::string b64Decode(std::string encoded) {
+		std::string result;
+		b64Decode(&encoded, &result);
+		return result;
+	}
+
+	inline std::string b64Encode(std::string source) {
+		std::string result;
+		b64Encode(&source, &result);
+		return result;
+	}
+
+	inline bool b64Validate(std::string* encoded) {
+		
+		for (auto bcr : *encoded) {
+			if (!isalnum(bcr) && !((bcr == '+') || (bcr == '/') || (bcr == '=')))
+				return false;
+		}
+			
+		return true;
+	}
 
 }
 
