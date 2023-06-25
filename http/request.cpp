e4af7144e00c50796295fb9abca0a2c5a29ec116
maddsua <maddsuadev@proton.me>
2023-06-25 22:13:39 +0300

    Create request.cpp

---
+++ http/request.cpp
@@ -0,0 +1,66 @@
+#include "http.hpp"
+#include <algorithm>
+#include <array>
+#include <cstdio>
+#include <string.h>
+
+HTTP::Request::Request(std::vector<uint8_t>& httpHead) {
+
+	static const char* patternEndline = "\r\n";
+	static const char* patternEndHeader = "\r\n\r\n";
+
+	try {
+
+		auto httpHeaderLineEnd = std::search(httpHead.begin(), httpHead.end(), patternEndline, patternEndline + strlen(patternEndline));
+		auto httpHeaderEnd = std::search(httpHead.begin(), httpHead.end(), patternEndHeader, patternEndHeader + strlen(patternEndHeader));
+		auto headerLineItems = stringSplit(std::string(httpHead.begin(), httpHeaderLineEnd), " ");
+
+		const auto method = headerLineItems.at(0);
+		this->_method = stringToUpperCase(method);
+
+		const auto path = headerLineItems.at(1);
+		auto pathSearchQueryIdx = path.find_first_of('?');
+		if (pathSearchQueryIdx != std::string::npos) {
+			this->_path = path.substr(0, path.find_first_of('?'));
+			this->_searchParams = URLSearchParams(path.substr(path.find_first_of('?') + 1));
+		} else this->_path = path;
+
+		if (httpHeaderLineEnd != httpHeaderEnd)
+			this->_headers = Headers(std::string(httpHeaderLineEnd + strlen(patternEndline), httpHeaderEnd));
+		
+
+	} catch(const std::exception& e) {
+		//std::cerr << e.what() << '\n';
+		//puts("ooooops!");
+		//	haha not doint anything lol
+	}
+
+}
+
+void HTTP::Request::setBody(std::vector<uint8_t>& content) {
+	this->_body = content;
+}
+void HTTP::Request::setBody(std::string& content) {
+	this->_body = std::vector<uint8_t>(content.begin(), content.end());
+}
+
+const std::string HTTP::Request::method() {
+	return this->_method;
+}
+const std::string HTTP::Request::path() {
+	return this->_path;
+}
+const HTTP::URLSearchParams HTTP::Request::searchParams() {
+	return this->_searchParams;
+}
+
+const HTTP::Headers HTTP::Request::headers() {
+	return this->_headers;
+}
+
+const std::vector<uint8_t> HTTP::Request::body() {
+	return this->_body;
+}
+const std::string HTTP::Request::text() {
+	return std::string(this->_body.begin(), this->_body.end());
+}
