4e5ae5b0c50c756c22f4ff7590655daccf4c038e
maddsua <maddsuadev@proton.me>
2023-06-27 09:38:06 +0300

    Update logs.cpp

--- server/logs.cpp
+++ server/logs.cpp
@@ -16,10 +16,14 @@ bool Lambda::Server::hasNewLogs() {
 }
 
 std::vector<Lambda::LogEntry> Lambda::Server::logs() {
+
 	if (!hasNewLogs()) return {};
+
 	std::lock_guard<std::mutex>lock(mtLock);
+
 	auto queueCopy = std::vector<LogEntry>(this->logQueue.begin(), this->logQueue.end());
 	this->logQueue.clear();
+
 	return queueCopy;
 }
 
@@ -27,9 +31,12 @@ std::vector<std::string> Lambda::Server::logsText() {
 	if (!hasNewLogs()) return {};
 
 	std::vector<std::string> logEntries;
+
 	for (auto& item : this->logQueue) {
-		logEntries.push_back(item.datetime + " " + item.message);
+		logEntries.push_back("[" + item.datetime + "] " + item.message);
 	}
-	
+
+	this->logQueue.clear();
+
 	return logEntries;
 }
