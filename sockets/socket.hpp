91297a2d9ee2dda2bb56ca8eeb1f4a4b1ea80a6e
maddsua <maddsuadev@proton.me>
2023-06-27 03:51:47 +0300

    Update socket.hpp

--- sockets/socket.hpp
+++ sockets/socket.hpp
@@ -31,17 +31,41 @@ namespace HTTPSocket {
 	static const size_t network_chunksize_header = 2048;
 	static const size_t network_chunksize_body = 131072;
 
-	HTTP::Request receiveMessage(SOCKET clientSocket);
-	bool sendMessage(SOCKET clientSocket, HTTP::Response& response);
+	enum Error {
+		HSOCKERR_OK = 0,
+		HSOCKERR_INIT = -1,
+		HSOCKERR_LHADDRESS = -2,
+		HSOCKERR_CREATE = -3,
+		HSOCKERR_BIND = -4,
+		HSOCKERR_LISTEN = -5,
+		HSOCKERR_SHUTDOWN = -6,
+		HSOCKERR_CLOSESOCK = -7,
+		HSOCKERR_SEND = -8,
+		HSOCKERR_SENDOPT = 10
+	};
+
+	struct OpStatus {
+		Error error = HSOCKERR_OK;
+		uint32_t code = HSOCKERR_OK;
+	};
+
+	HTTP::Request receiveMessage(SOCKET sockref);
+	OpStatus sendMessage(SOCKET sockref, HTTP::Response& response);
+	OpStatus createAndListen(SOCKET* sockPtr, const char* port);
+	OpStatus disconnect(SOCKET sockref);
+	OpStatus setConnectionTimeout(SOCKET sockref, uint32_t sockTimeoutMs);
 
 	class Server {
 		private:
 			SOCKET ListenSocket;
 			std::thread watchdogThread;
-			void connectionWatchdog();
-			void connectionHandler();
 			bool handlerDispatched;
 			bool running;
+
+			void connectionWatchdog();
+			void connectionHandler();
+
+			//ServerStat ();
 			
 		public:
 			Server();
