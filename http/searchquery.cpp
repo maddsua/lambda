7c2676244d67fee6d7c65dcc440bb6b7a1c68b91
maddsua <maddsuadev@proton.me>
2023-06-28 17:29:01 +0300

    Update searchquery.cpp

--- http/searchquery.cpp
+++ http/searchquery.cpp
@@ -1,14 +1,10 @@
 #include "./http.hpp"
 #include "../encoding/encoding.hpp"
 
-using namespace Lambda;
+using namespace Lambda::HTTP;
 
-Lambda::HTTP::URLSearchParams::URLSearchParams() {
-	//	do nothing again
-}
+void URLSearchParams::fromHref(const std::string& URLString) {
 
-Lambda::HTTP::URLSearchParams::URLSearchParams(const std::string& URLString) {
-	
 	auto qmark = URLString.find_last_of('?');
 	auto queries = stringSplit(qmark != std::string::npos ? URLString.substr(qmark + 1) : URLString, "&");
 	
@@ -19,46 +15,41 @@ Lambda::HTTP::URLSearchParams::URLSearchParams(const std::string& URLString) {
 		auto query = stringSplit(item, "=");
 		if (query.size() < 2) continue;
 
-		auto key = query.at(0);
-		auto value = query.at(1);
+		const auto key = query.at(0);
+		const auto value = query.at(1);
 
-		Lambda::HTTP::stringTrim(key);
-		Lambda::HTTP::stringTrim(value);
-		Lambda::HTTP::stringToLowerCase(key);
-
-		data[key] = value;
+		data[stringToLowerCase(stringTrim(key))] = stringTrim(value);
 	}
-	
 }
 
-bool Lambda::HTTP::URLSearchParams::has(std::string key) {
+bool URLSearchParams::has(std::string key) {
 	stringToLowerCase(key);
 	return this->data.find(key) != this->data.end();
 }
 
-void Lambda::HTTP::URLSearchParams::set(std::string key, const std::string& value) {
+void URLSearchParams::set(std::string key, const std::string& value) {
 	stringToLowerCase(key);
 	this->data[key] = Encoding::encodeURIComponent(value);
 }
 
-bool Lambda::HTTP::URLSearchParams::append(std::string key, const std::string& value) {
+bool URLSearchParams::append(std::string key, const std::string& value) {
 	if (has(key)) return false;
 	set(key, value);
 	return true;
 }
 
-std::string Lambda::HTTP::URLSearchParams::get(std::string key) {
+std::string URLSearchParams::get(std::string key) {
 	stringToLowerCase(key);
 	return has(key) ? std::string() : this->data[key];
 }
 
-void Lambda::HTTP::URLSearchParams::del(std::string key) {
+void URLSearchParams::del(std::string key) {
 	stringToLowerCase(key);
 	if (!has(key)) return;
 	this->data.erase(key);
 }
 
-std::string Lambda::HTTP::URLSearchParams::stringify() {
+std::string URLSearchParams::stringify() {
 	auto result = std::string();
 	for (auto item : this->data) {
 		result += (result.size() ? "&" : "") + item.first + "=" + item.second;
@@ -66,14 +57,14 @@ std::string Lambda::HTTP::URLSearchParams::stringify() {
 	return result;
 }
 
-std::vector<Lambda::HTTP::KVtype> Lambda::HTTP::URLSearchParams::entries() {
-	auto entries = std::vector<Lambda::HTTP::KVtype>();
+std::vector<KVtype> URLSearchParams::entries() {
+	auto entries = std::vector<KVtype>();
 	for (auto& item : this->data) {
 		entries.push_back({ item.first, item.second });
 	}
 	return entries;
 }
 
-size_t Lambda::HTTP::URLSearchParams::length() {
+size_t URLSearchParams::length() {
 	return this->data.size();
 }
