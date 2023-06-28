f2a08bacb2024d11872ec5a53009ec00f47e39b7
maddsua <maddsuadev@proton.me>
2023-06-28 15:18:30 +0300

    Update server.cpp

--- tests/server.cpp
+++ tests/server.cpp
@@ -1,5 +1,4 @@
 #include "../server/server.hpp"
-#include <windows.h>
 
 using namespace Lambda;
 
@@ -20,7 +19,7 @@ int main() {
 			puts(HTTP::stringJoin(server.logsText(), "\n").c_str());
 		}
 
-		Sleep(10);
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 	
 	return 0;
