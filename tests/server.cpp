8977e3fcb7c144ad020148a4789059c498939f06
maddsua <maddsuadev@proton.me>
2023-06-28 15:15:21 +0300

    Update server.cpp

--- tests/server.cpp
+++ tests/server.cpp
@@ -5,7 +5,7 @@ using namespace Lambda;
 
 HTTP::Response callback(HTTP::Request& request, Lambda::Context& context) {
 	puts(request.path().c_str());
-	return HTTP::Response({{"x-serverless", "true"}}, "success!");
+	return HTTP::Response({{"x-serverless", "true"}}, "success! your user-agent is: " + request.headers().get("user-agent"));
 };
 
 int main() {
