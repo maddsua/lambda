af1d4fbdc3364389cf0a74eb2e255a945e31048b
maddsua <maddsuadev@proton.me>
2023-07-04 02:11:36 +0300

    Update kvstore.hpp
    Update kv.cpp
    
    Update kv.cpp
    
    Update kv.cpp
    
    Update kvstore.hpp
    
    Update kv.cpp

--- storage/kv.cpp
+++ storage/kv.cpp
@@ -5,36 +5,56 @@
 using namespace Lambda;
 using namespace Lambda::Storage;
 
+
 const char magicString[] = "lambda kv bundle";
 
-struct KVStoreFileHeader {
+struct StoreFileHeader {
 	char magicbytes[sizeof(magicString)];
 	uint32_t headerSize;
 };
+
+struct StoreMainHeader {
+	uint16_t version;
+	uint8_t compression;
+};
+
+struct StoreRecordHeader {
+	uint64_t created;
+	uint64_t modified;
+	uint16_t keySize;
+	uint16_t valueSize;
+	uint8_t type;
+};
+
+enum StoreRecordType {
+	KVENTRY_DATA = 0,
+	KVENTRY_META = 1,
+};
+
 
 bool KV::has(const std::string& key) {
 	return this->data.find(key) != this->data.end();
 }
 
-KVEntry KV::get(const std::string& key) {
-	std::lock_guard <std::mutex> lock (threadLock);
+KVMapEntry KV::get(const std::string& key) {
+	std::lock_guard<std::mutex>lock(threadLock);
 	if (!this->has(key)) return {};
 	return this->data.at(key);
 }
 
 std::string KV::getValue(const std::string& key) {
-	std::lock_guard <std::mutex> lock (threadLock);
+	std::lock_guard<std::mutex>lock(threadLock);
 	if (!this->has(key)) return {};
 	return this->data.at(key).value;
 }
 
 void KV::set(const std::string& key, const std::string& value) {
 
-	KVEntry newEntry;
+	KVMapEntry newEntry;
 	newEntry.value = value;
 	newEntry.modified = time(nullptr);
 
-	std::lock_guard <std::mutex> lock (threadLock);
+	std::lock_guard<std::mutex>lock(threadLock);
 
 	if (this->has(key)) {
 		newEntry.created = get(key).created;
@@ -44,48 +64,68 @@ void KV::set(const std::string& key, const std::string& value) {
 }
 
 bool KV::del(const std::string& key) {
-	std::lock_guard <std::mutex> lock (threadLock);
+	std::lock_guard<std::mutex>lock(threadLock);
 	if (!this->has(key)) return false;
 	this->data.erase(key);
 	return true;
 }
 
 bool KV::move(const std::string& key, const std::string& newKey) {
-	std::lock_guard <std::mutex> lock (threadLock);
+	std::lock_guard<std::mutex>lock(threadLock);
 	if (!this->has(key)) return false;
 	this->data[newKey] = this->data.extract(key).mapped();
 	return true;
 }
 
-Lambda::Error KV::exportBundle(const char* filepath, uint8_t compression) {
+std::vector<KVEntry> KV::entries() {
+
+	std::vector<KVEntry> result;
+	std::lock_guard<std::mutex>lock(threadLock);
+
+	for (const auto& item : this->data) {
+
+		KVEntry temp;
+
+		temp.key = item.first;
+		temp.value = item.second.value;
+		temp.created = item.second.created;
+		temp.modified = item.second.modified;
+		
+		result.push_back(std::move(temp));
+	}
+
+	return result;
+}
+
+Lambda::Error KV::exportStore(const char* filepath) {
 
 	auto localfile = std::ofstream(filepath, std::ios::binary);
 	if (!localfile.is_open()) return { std::string("Could not open file \"") + filepath + "\" for write" };
 
 	//	write file header
-	KVStoreFileHeader bHeader;
-	bHeader.headerSize = sizeof(KVStoreHeader);
-	memcpy(bHeader.magicbytes, magicString, sizeof(magicString));
-	localfile.write((const char*)&bHeader, sizeof(bHeader));
+	StoreFileHeader fHeader;
+	fHeader.headerSize = sizeof(StoreMainHeader);
+	memcpy(fHeader.magicbytes, magicString, sizeof(magicString));
+	localfile.write((const char*)&fHeader, sizeof(fHeader));
 
-	KVStoreHeader sHeader;
-	sHeader.version = 1;
-	sHeader.compression = compression;
-	localfile.write((const char*)&sHeader, sizeof(sHeader));
+	StoreMainHeader mHeader;
+	mHeader.version = 1;
+	mHeader.compression = 0;
+	localfile.write((const char*)&mHeader, sizeof(mHeader));
 
 	//	lock data store and begin export
-	std::lock_guard <std::mutex> lock (threadLock);
+	std::lock_guard<std::mutex>lock(threadLock);
 
 	for (const auto& item : this->data) {
 
-		KVRecordHeader header;
-		header.created = item.second.created;
-		header.modified = item.second.modified;
-		header.keySize = item.first.size();
-		header.type = KVENTRY_DATA;
-		header.valueSize = item.second.value.size();
+		StoreRecordHeader rHeader;
+		rHeader.created = item.second.created;
+		rHeader.modified = item.second.modified;
+		rHeader.keySize = item.first.size();
+		rHeader.type = KVENTRY_DATA;
+		rHeader.valueSize = item.second.value.size();
 
-		localfile.write((const char*)&header, sizeof(header));
+		localfile.write((const char*)&rHeader, sizeof(rHeader));
 		localfile.write(item.first.data(), item.first.size());
 		localfile.write(item.second.value.data(), item.second.value.size());
 	}
@@ -99,30 +139,30 @@ Lambda::Error KV::importStore(const char* filepath) {
 	auto localfile = std::ifstream(filepath, std::ios::binary);
 	if (!localfile.is_open()) return { std::string("Could not open file \"") + filepath + "\" for read" };
 
-	KVStoreFileHeader bHeader;
-	memset(&bHeader, 0, sizeof(bHeader));
-	localfile.read((char*)&bHeader, sizeof(bHeader));
+	StoreFileHeader fHeader;
+	memset(&fHeader, 0, sizeof(fHeader));
+	localfile.read((char*)&fHeader, sizeof(fHeader));
 
-	KVStoreHeader sHeader;
-	memset(&sHeader, 0, sizeof(sHeader));
-	localfile.read((char*)&sHeader, bHeader.headerSize > sizeof(sHeader) ? sizeof(sHeader) : bHeader.headerSize);
+	StoreMainHeader mHeader;
+	memset(&mHeader, 0, sizeof(mHeader));
+	localfile.read((char*)&mHeader, fHeader.headerSize > sizeof(mHeader) ? sizeof(mHeader) : fHeader.headerSize);
 
-	if (bHeader.headerSize > sizeof(sHeader)) return { "Unsupported bundle version" };
+	if (fHeader.headerSize > sizeof(mHeader)) return { "Unsupported bundle version" };
 
 	while (!localfile.eof()) {
-		KVRecordHeader header;
-		localfile.read((char*)&header, sizeof(header));
+		StoreRecordHeader rheader;
+		localfile.read((char*)&rheader, sizeof(rheader));
 
 		std::string entryKey;
-		entryKey.resize(header.keySize);
+		entryKey.resize(rheader.keySize);
 
-		KVEntry entry;
-		entry.created = header.created;
-		entry.modified = header.modified;
-		entry.value.resize(header.valueSize);
+		KVMapEntry entry;
+		entry.created = rheader.created;
+		entry.modified = rheader.modified;
+		entry.value.resize(rheader.valueSize);
 
-		localfile.read((char*)entryKey.data(), header.keySize);
-		localfile.read((char*)entry.value.data(), header.valueSize);
+		localfile.read((char*)entryKey.data(), rheader.keySize);
+		localfile.read((char*)entry.value.data(), rheader.valueSize);
 		this->data[entryKey] = entry;
 	}
 
--- storage/kvstore.hpp
+++ storage/kvstore.hpp
@@ -10,53 +10,31 @@
 
 namespace Lambda::Storage {
 
-	struct KVEntry {
+	struct KVMapEntry {
 		std::string value;
 		time_t created = 0;
 		time_t modified = 0;
 	};
 
-	struct KVStoreHeader {
-		uint16_t version;
-		uint8_t compression;
-	};
-
-	struct KVRecordHeader {
-		uint8_t type;
-		uint64_t created;
-		uint64_t modified;
-		uint16_t keySize;
-		uint16_t valueSize;
-	};
-
-	enum KVEntryTypes {
-		KVENTRY_DATA = 0,
-		KVENTRY_META = 1,
-	};
-
-	enum KVStoreCompression {
-		KVCOMPRESS_NONE = 0,
-		KVCOMPRESS_BR_CHUNK = 1,
+	struct KVEntry : public KVMapEntry {
+		std::string key;
 	};
 
 	class KV {
 		private:
-			std::unordered_map<std::string, KVEntry> data;
+			std::unordered_map<std::string, KVMapEntry> data;
 			std::mutex threadLock;
 
 		public:
 			void set(const std::string& key, const std::string& value);
 			bool has(const std::string& key);
-			KVEntry get(const std::string& key);
+			KVMapEntry get(const std::string& key);
 			std::string getValue(const std::string& key);
 			bool move(const std::string& key, const std::string& newKey);
 			bool del(const std::string& key);
+			std::vector<KVEntry> entries();
 
-			Lambda::Error exportBundle(const char* filepath, uint8_t compression);
-			Lambda::Error exportStore(const char* filepath) {
-				return exportBundle(filepath, KVCOMPRESS_NONE);
-			};
-
+			Lambda::Error exportStore(const char* filepath);
 			Lambda::Error importStore(const char* filepath);
 	};
 };
