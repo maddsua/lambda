42d1a1ea69a7a5f30a7c87e944c003f7e46fa24f
maddsua <maddsuadev@proton.me>
2023-07-13 21:24:12 +0300

    Create responses.cpp

---
+++ server/responses.cpp
@@ -0,0 +1,38 @@
+#include "./responses.hpp"
+
+using namespace Lambda;
+
+//	these variables (pointers, whatever, I'm too dumb for C++ anyway) should be imported from servicepage.html (servicepage.o)
+extern char _binary_resources_static_html_servicepage_html_start;
+extern char _binary_resources_static_html_servicepage_html_end;
+
+static const auto template_start = &_binary_resources_static_html_servicepage_html_start;
+static const auto template_end = &_binary_resources_static_html_servicepage_html_end;
+static const auto pageTemplate = std::string(template_start, template_end - template_start);
+
+typedef std::vector<std::pair<std::string, std::string>> TemplateContent;
+
+std::string populateTemplate(const TemplateContent& content) {
+
+	auto result = pageTemplate;
+	auto matchIdx = std::string::npos;
+
+	for (const auto& item : content) {
+		while ((matchIdx = result.find(item.first)) != std::string::npos) {
+			result.replace(matchIdx, item.first.size(), item.second);
+		}
+	}
+
+	return result;
+}
+
+HTTP::Response Responses::serviceResponse(uint16_t httpStatus, const std::string& text) {
+
+	TemplateContent content = {
+		{ "${html_svcpage_statuscode}", std::to_string(httpStatus) },
+		{ "${html_svcpage_statustext}", HTTP::statusText(httpStatus) },
+		{ "${html_svcpage_message_text}", text }
+	};
+
+	return HTTP::Response(httpStatus, {}, populateTemplate(content));
+}
