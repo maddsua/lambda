75a17e8557842582861b4e0fc319d1eef731147c
maddsua <maddsuadev@proton.me>
2023-11-27 15:01:39 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,19 +3,19 @@
 
 int main(int argc, char const *argv[]) {
 
-	auto url = new HTTP::URL("https://user:password@example.com:443/document?search=query#fragment");
+	HTTP::URL url = "https://user:password@example.com:443/document?search=query#fragment";
 
-	std::cout << "hash: " << url->hash << std::endl;
-	std::cout << "search: " << url->searchParams.stringify() << std::endl;
-	std::cout << "protocol: " << url->protocol << std::endl;
-	std::cout << "host: " << url->host << std::endl;
-	std::cout << "hostname: " << url->hostname << std::endl;
-	std::cout << "port: " << url->port << std::endl;
-	std::cout << "pathname: " << url->pathname << std::endl;
-	std::cout << "username: " << url->username << std::endl;
-	std::cout << "password: " << url->password << std::endl;
+	std::cout << "hash: " << url.hash << std::endl;
+	std::cout << "search: " << url.searchParams.stringify() << std::endl;
+	std::cout << "protocol: " << url.protocol << std::endl;
+	std::cout << "host: " << url.host << std::endl;
+	std::cout << "hostname: " << url.hostname << std::endl;
+	std::cout << "port: " << url.port << std::endl;
+	std::cout << "pathname: " << url.pathname << std::endl;
+	std::cout << "username: " << url.username << std::endl;
+	std::cout << "password: " << url.password << std::endl;
 
-	std::cout << "full: " << url->href() << std::endl;
+	std::cout << "full: " << url.href() << std::endl;
 
 	/* code */
 	return 0;
