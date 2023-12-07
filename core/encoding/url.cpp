25634d9b854299df66f76a444aad95cfd63f98ce
maddsua <maddsuadev@proton.me>
2023-12-07 21:13:37 +0200

    Update url.cpp

--- core/encoding/url.cpp
+++ core/encoding/url.cpp
@@ -28,3 +28,26 @@ std::string Encoding::encodeURIComponent(const std::string& input) {
 
 	return std::move(result);
 }
+
+std::string Encoding::decodeURIComponent(const std::string& input) {
+
+	std::string result;
+	result.reserve(input.size());
+
+	for (size_t i = 0; i < input.size(); i++) {
+		
+		if (input[i] == '%') {
+
+			HexByte temp;
+			strncpy(temp.string, input.substr(i + 1, 2).c_str(), sizeof(temp.string));
+			result.push_back(decodeHexByte(temp));
+
+			i += 2;
+			continue;
+		}
+
+		result.push_back(input[i]);
+	}
+
+	return std::move(result);
+}
