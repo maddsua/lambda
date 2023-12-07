afed6ccda2b8a306f8dbc5979619977d9935e69c
maddsua <maddsuadev@proton.me>
2023-12-07 20:49:55 +0200

    Update encoding.hpp

--- core/encoding.hpp
+++ core/encoding.hpp
@@ -9,6 +9,20 @@ namespace Encoding {
 	std::string toBase64(const std::string& data);
 	std::string fromBase64(const std::string& data);
 
+	union HexByte {
+		char string[3];
+		struct data {
+			char first;
+			char second;
+		} data;
+	};
+
+	HexByte encodeHexByte(char databyte);
+	std::string toHex(const std::string& input);
+
+	char decodeHexByte(HexByte hexbyte);
+	std::string fromHex(const std::string& input);
+
 };
 
 #endif
