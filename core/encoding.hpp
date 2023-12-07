bce7373835fbbc443d047522857ace6a6fccf43d
maddsua <maddsuadev@proton.me>
2023-12-07 19:16:18 +0200

    Create encoding.hpp

---
+++ core/encoding.hpp
@@ -0,0 +1,14 @@
+#ifndef _OCTOPUSS_CORE_ENCODING_
+#define _OCTOPUSS_CORE_ENCODING_
+
+#include "./polyfill.hpp"
+#include "./http.hpp"
+
+namespace Encoding {
+
+	std::string decodeBase64(const std::string& data);
+	std::string encodeBase64(const std::string& data);
+
+};
+
+#endif
