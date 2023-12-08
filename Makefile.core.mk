51f84e7527bf4972237f4eba6f9e284684f01124
maddsua <maddsuadev@proton.me>
2023-12-08 21:27:07 +0200

    Squashed commit of the following:
    commit 8ba485e8a43c71fb9efddc1567972f0f43e5d8f5
    Author: maddsua <maddsuadev@proton.me>
    Date:   Fri Dec 8 20:00:47 2023 +0200
    
        Update Makefile.core.mk
    
    commit 1d98231f48843006ae5d720926c5612fbc19dc53
    Author: maddsua <maddsuadev@proton.me>
    Date:   Fri Dec 8 20:00:45 2023 +0200
    
        Update Makefile
    
    commit cd042c2441159c25017dc4e5740b869d4c95d350
    Merge: 9437efb c83e43e
    Author: maddsua <maddsuadev@proton.me>
    Date:   Fri Dec 8 19:52:33 2023 +0200
    
        Merge branch 'next' into 57-rebuild-build-system
    
    commit 9437efb9a894b442fdb55dcf6e1e76b304d5ff14
    Author: maddsua <maddsuadev@proton.me>
    Date:   Fri Dec 8 19:51:35 2023 +0200
    
        Update Makefile.core.mk
    
    commit e29402742f3066724d5f849064ac84ae81578a7d
    Author: maddsua <maddsuadev@proton.me>
    Date:   Fri Dec 8 19:48:05 2023 +0200
    
        Update Makefile
    
    commit d6e68e88219ec36c5c721f82ae58b390147726ed
    Author: maddsua <maddsuadev@proton.me>
    Date:   Fri Dec 8 19:48:03 2023 +0200
    
        Create Makefile.core.mk

---
+++ Makefile.core.mk
@@ -0,0 +1,53 @@
+CORE_OBJ_TARGET	=	core.o
+
+OBJ_POLYFILL	=	core/polyfill/strings.o core/polyfill/date.o core/polyfill/mimetype.o
+OBJ_HTTP		=	core/http/cookie.o core/http/headers.o core/http/kvcontainer.o core/http/url.o core/http/urlsearchparams.o core/http/method.o core/http/status.o
+OBJ_ENCODING	=	core/encoding/base64.o core/encoding/hex.o core/encoding/url.o
+
+CORE_OBJ_DEPS	=	$(OBJ_POLYFILL) $(OBJ_HTTP) $(OBJ_ENCODING)
+
+# target object
+$(CORE_OBJ_TARGET): $(CORE_OBJ_DEPS)
+	ld -relocatable $(CORE_OBJ_DEPS) -o $(CORE_OBJ_TARGET)
+
+# polyfill stuff
+core/polyfill/strings.o: core/polyfill/strings.cpp
+	g++ -c core/polyfill/strings.cpp -o core/polyfill/strings.o $(CFLAGS)
+
+core/polyfill/date.o: core/polyfill/date.cpp
+	g++ -c core/polyfill/date.cpp -o core/polyfill/date.o $(CFLAGS)
+
+core/polyfill/mimetype.o: core/polyfill/mimetype.cpp
+	g++ -c core/polyfill/mimetype.cpp -o core/polyfill/mimetype.o $(CFLAGS)
+
+# http stuff
+core/http/cookie.o: core/http/cookie.cpp
+	g++ -c core/http/cookie.cpp -o core/http/cookie.o $(CFLAGS)
+
+core/http/headers.o: core/http/headers.cpp
+	g++ -c core/http/headers.cpp -o core/http/headers.o $(CFLAGS)
+
+core/http/kvcontainer.o: core/http/kvcontainer.cpp
+	g++ -c core/http/kvcontainer.cpp -o core/http/kvcontainer.o $(CFLAGS)
+
+core/http/url.o: core/http/url.cpp
+	g++ -c core/http/url.cpp -o core/http/url.o $(CFLAGS)
+
+core/http/urlsearchparams.o: core/http/urlsearchparams.cpp
+	g++ -c core/http/urlsearchparams.cpp -o core/http/urlsearchparams.o $(CFLAGS)
+
+core/http/method.o: core/http/method.cpp
+	g++ -c core/http/method.cpp -o core/http/method.o $(CFLAGS)
+
+core/http/status.o: core/http/status.cpp
+	g++ -c core/http/status.cpp -o core/http/status.o $(CFLAGS)
+
+# encoding stuff
+core/encoding/base64.o: core/encoding/base64.cpp
+	g++ -c core/encoding/base64.cpp -o core/encoding/base64.o $(CFLAGS)
+
+core/encoding/hex.o: core/encoding/hex.cpp
+	g++ -c core/encoding/hex.cpp -o core/encoding/hex.o $(CFLAGS)
+
+core/encoding/url.o: core/encoding/url.cpp
+	g++ -c core/encoding/url.cpp -o core/encoding/url.o $(CFLAGS)
