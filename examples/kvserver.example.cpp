6cf5de2039c0f7235106ee9ae6d930d5a1b8be00
maddsua <maddsuadev@proton.me>
2023-12-15 17:46:28 +0200

    Update kvserver.example.cpp

--- examples/kvserver.example.cpp
+++ examples/kvserver.example.cpp
@@ -6,12 +6,36 @@ using namespace Lambda;
 
 int main(int argc, char const *argv[]) {
 
+	auto storage = Lambda::Storage::LocalStorage();
+
 	auto handler = [&](const Request& req, const Context& context) {
-		context.console.log({"Serving rq for:", req.url.pathname});
 
-		if (req.body.size()) context.console.log({"Request payload", req.body.text()});
+		//	get record key
+		auto key = req.url.searchParams.get("record");
+		if (!key.size()) return HTTP::Response(HTTP::Status(400), "no record key provided");
 
-		return HTTP::Response("status report: live");
+		switch (req.method) {
+
+			case HTTP::Methods::GET: {
+				auto value = storage.getItem(key);
+				if (!value.size()) return HTTP::Response(HTTP::Status(404), "object not found");
+				return HTTP::Response(value);
+			} break;
+
+			case HTTP::Methods::POST: {
+				storage.setItem(key, req.body.text());
+				return HTTP::Response(204, "created");
+			} break;
+
+			case HTTP::Methods::DELETE: {
+				storage.removeItem(key);
+				return HTTP::Response(200, "deleted");
+			} break;
+			
+			default: break;
+		}
+
+		return HTTP::Response(HTTP::Status(400), "bad request");
 	};
 
 	HttpServerConfig initparams;
