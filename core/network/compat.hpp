d69c5f85bf37958b638222ad660cd1f7ed096c2d
maddsua <maddsuadev@proton.me>
2024-01-23 20:50:00 +0200

    Update compat.hpp

--- core/network/compat.hpp
+++ core/network/compat.hpp
@@ -1,14 +1,28 @@
 #ifndef  __LIB_MADDSUA_LAMBDA_NETWORK_COMPAT__
 #define  __LIB_MADDSUA_LAMBDA_NETWORK_COMPAT__
 
-#include <stdint.h>
+	#include <stdint.h>
 
-#ifndef SOCKET
-	typedef uint64_t SOCKET;
-#endif
+	#ifdef _WIN32
 
-#ifndef INVALID_SOCKET
-	#define INVALID_SOCKET (-1UL)
-#endif
+		#ifndef SOCKET
+			typedef uint64_t SOCKET;
+		#endif
+
+		#ifndef INVALID_SOCKET
+			#define INVALID_SOCKET (-1ULL)
+		#endif
+
+	#else
+
+		#ifndef SOCKET
+			typedef int SOCKET;
+		#endif
+
+		#ifndef INVALID_SOCKET
+			#define INVALID_SOCKET (-1)
+		#endif
+
+	#endif
 
 #endif
