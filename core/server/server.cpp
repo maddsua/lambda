693580465b734734cd3952c9455eea1060156c8f
maddsua <maddsuadev@proton.me>
2024-01-24 01:27:04 +0200

    Update server.cpp

--- core/server/server.cpp
+++ core/server/server.cpp
@@ -21,6 +21,57 @@ ServerInstance::ServerInstance(HandlerFunction handlerInit, ServerConfig init) {
 	auto tempListener = Network::TCP::ListenSocket(listenInitOpts);
 	this->listener = new Network::TCP::ListenSocket(std::move(tempListener));
 
+	auto connectionHandler = [&](Network::TCP::Connection&& conn) {
+
+		const auto& connInfo = conn.getInfo();
+
+		try {
+
+			if (this->config.loglevel.logConnections) fprintf(stdout,
+				"%s %s:%i connected on %i\n",
+				Date().toHRTString().c_str(),
+				connInfo.remoteAddr.port,
+				connInfo.remoteAddr.hostname.c_str(),
+				connInfo.hostPort
+			);
+
+			Server::serveHTTP(std::move(conn), this->handler, {
+				this->config.loglevel,
+				this->config.transport
+			});
+
+		} catch(const std::exception& e) {
+
+			if (this->terminated) return;
+
+			if (this->config.loglevel.logRequests) fprintf(stderr,
+				"%s [Service] Connection to %s terminated: %s\n",
+				Date().toHRTString().c_str(),
+				connInfo.remoteAddr.hostname.c_str(),
+				e.what()
+			);
+
+		} catch(...) {
+
+			if (this->terminated) return;
+
+			if (this->config.loglevel.logRequests) fprintf(stderr,
+				"%s [Service] Connection to %s terminated (unknown error)\n",
+				Date().toHRTString().c_str(),
+				connInfo.remoteAddr.hostname.c_str()
+			);
+		}
+
+		if (this->config.loglevel.logConnections) fprintf(stdout,
+			"%s %s:%i disconnected from %i\n",
+			Date().toHRTString().c_str(),
+			connInfo.remoteAddr.port,
+			connInfo.remoteAddr.hostname.c_str(),
+			connInfo.hostPort
+		);
+
+	};
+
 	this->watchdogWorker = std::thread([&]() {
 
 		while (!this->terminated && this->listener->ok()) {
@@ -28,50 +79,7 @@ ServerInstance::ServerInstance(HandlerFunction handlerInit, ServerConfig init) {
 			try {
 
 				auto nextConn = this->listener->acceptConnection();
-
-				auto connectionWorker = std::thread([&](Network::TCP::Connection&& conn) {
-
-					const auto& connfinfo = conn.getInfo();
-					std::string contextID = "noid";
-
-					try {
-
-						if (this->config.loglevel.logConnections) {
-							printf("%s %s opens %s\n",
-								Date().toHRTString().c_str(),
-								connfinfo.remoteAddr.hostname.c_str(),
-								contextID.c_str()
-							);
-						}
-
-						Server::serveHTTP(std::move(conn), this->handler, {
-							this->config.loglevel,
-							this->config.transport
-						});
-
-					} catch(const std::exception& e) {
-
-						if (this->terminated) return;
-
-						if (this->config.loglevel.logRequests) {
-							fprintf(stderr, "%s [Service] http handler %s crashed: %s\n", Date().toHRTString().c_str(), contextID.c_str(), e.what()); 
-						}
-
-					} catch(...) {
-
-						if (this->terminated) return;
-
-						if (this->config.loglevel.logRequests) {
-							fprintf(stderr, "%s [Service] http handler %s crashed with unknown error\n", Date().toHRTString().c_str(), contextID.c_str()); 
-						}
-					}
-
-					if (this->config.loglevel.logConnections) {
-						printf("%s %s was closed\n", Date().toHRTString().c_str(), contextID.c_str());
-					}
-
-				}, std::move(nextConn));
-
+				auto connectionWorker = std::thread(connectionHandler, std::move(nextConn));
 				connectionWorker.detach();
 
 			} catch(const std::exception& e) {
