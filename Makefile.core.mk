e816fa4c8df759ec27bacbc749b09c348fcd3ce8
maddsua <maddsuadev@proton.me>
2024-01-23 19:50:15 +0200

    Update Makefile.core.mk

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -6,7 +6,7 @@ LIB_CORE_POLYFILL		=	core/polyfill.a
 LIB_CORE_POLYFILL_DEPS	=	core/polyfill/strings.o core/polyfill/date.o core/polyfill/mimetype.o core/polyfill/uid.o
 
 LIB_CORE_HTTP			=	core/http.a
-LIB_CORE_HTTP_DEPS		=	core/http/cookie.o core/http/kvcontainer.o core/http/url.o core/http/urlsearchparams.o core/http/method.o core/http/status.o
+LIB_CORE_HTTP_DEPS		=	core/http/cookies.o core/http/kvcontainer.o core/http/url.o core/http/urlsearchparams.o core/http/method.o core/http/status.o
 
 LIB_CORE_ENCODING		=	core/encoding.a
 LIB_CORE_ENCODING_DEPS	=	core/encoding/base64.o core/encoding/hex.o core/encoding/url.o
