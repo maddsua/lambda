dec8c54d0b88130c54e8c7c45e1f21c650df6d7d
maddsua <maddsuadev@proton.me>
2024-01-24 01:29:54 +0200

    Update date.cpp

--- core/polyfill/date.cpp
+++ core/polyfill/date.cpp
@@ -9,58 +9,58 @@
 
 using namespace Lambda;
 
-Date::Date() {
+Date::Date() noexcept {
 	this->epoch = std::time(nullptr);
 	gmtime_r(&this->epoch, &this->timestruct);
 }
 
-Date::Date(time_t epoch) {
+Date::Date(time_t epoch) noexcept {
 	this->epoch = epoch;
 	gmtime_r(&epoch, &this->timestruct);
 }
 
-time_t Date::getTime() {
+time_t Date::getTime() const noexcept {
 	return this->epoch;
 }
 
-std::string Date::toUTCString() {
+std::string Date::toUTCString() const noexcept {
 	char timebuff[64];
 	strftime(timebuff, sizeof(timebuff), "%a, %d %b %Y %H:%M:%S GMT", &this->timestruct);
 	return timebuff;
 }
 
-std::string Date::toHRTString() {
+std::string Date::toHRTString() const noexcept {
 	char timebuff[32];
 	strftime(timebuff, sizeof(timebuff), "%d %b %Y %H:%M:%S", &this->timestruct);
 	return std::string(timebuff);
 }
 
-std::string Date::getDate() {
+std::string Date::getDate() const noexcept {
 	char timebuff[64];
 	strftime(timebuff, sizeof(timebuff), "%a, %d %b %Y", &this->timestruct);
 	return timebuff;
 }
 
-int Date::getSeconds() {
+int Date::getSeconds() const noexcept {
 	return this->timestruct.tm_sec;
 }
 
-int Date::getMonth() {
+int Date::getMonth() const noexcept {
 	return this->timestruct.tm_mon;
 }
 
-int Date::getMinutes() {
+int Date::getMinutes() const noexcept {
 	return this->timestruct.tm_min;
 }
 
-int Date::getHours() {
+int Date::getHours() const noexcept {
 	return this->timestruct.tm_hour;
 }
 
-int Date::getYear() {
+int Date::getYear() const noexcept {
 	return this->timestruct.tm_year;
 }
 
-int Date::getDay() {
+int Date::getDay() const noexcept {
 	return this->timestruct.tm_mday;
 }
