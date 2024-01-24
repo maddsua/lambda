6ff230cf089ee45ac86d1c9e2ad3d07c8f1cc933
maddsua <maddsuadev@proton.me>
2024-01-24 09:08:26 +0200

    Create router.hpp

---
+++ core/server/router.hpp
@@ -0,0 +1,18 @@
+#ifndef __LIB_MADDSUA_LAMBDA_CORE_SERVER_ROUTER__
+#define __LIB_MADDSUA_LAMBDA_CORE_SERVER_ROUTER__
+
+#include "../network/tcp/connection.hpp"
+#include "../network/tcp/listen.hpp"
+#include "./server.hpp"
+
+#include <future>
+#include <optional>
+#include <queue>
+
+namespace Lambda::Server {
+
+	
+
+};
+
+#endif
