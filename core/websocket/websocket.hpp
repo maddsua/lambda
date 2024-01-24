40b2ac388a3eda0fe75cac1d29187ab8b691acf0
maddsua <maddsuadev@proton.me>
2024-01-24 04:53:33 +0200

    Update websocket.hpp

--- core/websocket/websocket.hpp
+++ core/websocket/websocket.hpp
@@ -13,25 +13,11 @@ namespace Lambda::Websocket {
 			bool m_binary;
 
 		public:
-			Message(const std::string& init) {
-				this->m_binary = false;
-				this->m_buffer = std::vector<uint8_t>(init.begin(), init.end());
-			}
-			Message(const std::vector<uint8_t>& init) : m_buffer(init) {
-				this->m_binary = true;
-			}
-
-			const std::vector<uint8_t>& data() const noexcept {
-				return this->m_buffer;
-			}
-
-			std::string text() const {
-				return std::string(this->m_buffer.begin(), this->m_buffer.end());
-			}
-
-			bool isBinary() const noexcept {
-				return this->m_binary;
-			}
+			Message(const std::string& init);
+			Message(const std::vector<uint8_t>& init);
+			const std::vector<uint8_t>& data() const noexcept;
+			std::string text() const;
+			bool isBinary() const noexcept;
 	};
 
 };
