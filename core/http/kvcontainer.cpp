c382a5db31cba14b38de13af2d453d52b48c97cb
maddsua <maddsuadev@proton.me>
2024-01-24 08:03:28 +0200

    Update kvcontainer.cpp

--- core/http/kvcontainer.cpp
+++ core/http/kvcontainer.cpp
@@ -4,48 +4,48 @@
 using namespace Lambda::HTTP;
 
 KVContainer::KVContainer(const KVContainer& other) {
-	this->data = other.data;
+	this->m_data = other.m_data;
 }
 
-KVContainer::KVContainer(const std::vector<KVpair>& entries) {
+KVContainer::KVContainer(const std::initializer_list<KVpair>& init) {
 
-	for (const auto& entry : entries) {
+	for (const auto& entry : init) {
 
-		const auto keyNormalized = Strings::toLowerCase(entry.key);
-		const auto element = this->data.find(keyNormalized);
+		const auto keyNormalized = Strings::toLowerCase(entry.first);
+		const auto element = this->m_data.find(keyNormalized);
 
-		if (element == this->data.end()) {
-			this->data[keyNormalized] = { entry.value };
+		if (element == this->m_data.end()) {
+			this->m_data[keyNormalized] = { entry.second };
 			continue;
 		}
 
-		element->second.push_back(entry.value);
+		element->second.push_back(entry.second);
 	}
 }
 
 bool KVContainer::has(const std::string& key) const {
 	const auto keyNormalized = Strings::toLowerCase(key);
-	return this->data.contains(keyNormalized);
+	return this->m_data.contains(keyNormalized);
 }
 
 void KVContainer::del(const std::string& key) {
 	const auto keyNormalized = Strings::toLowerCase(key);
-	if (!this->data.contains(keyNormalized)) return;
-	this->data.erase(keyNormalized);
+	if (!this->m_data.contains(keyNormalized)) return;
+	this->m_data.erase(keyNormalized);
 }
 
 void KVContainer::set(const std::string& key, const std::string value) {
 	const auto keyNormalized = Strings::toLowerCase(key);
-	this->data[keyNormalized] = { value };
+	this->m_data[keyNormalized] = { value };
 }
 
 void KVContainer::append(const std::string& key, const std::string value) {
 
 	const auto keyNormalized = Strings::toLowerCase(key);
-	const auto element = this->data.find(keyNormalized);
+	const auto element = this->m_data.find(keyNormalized);
 
-	if (element == this->data.end()) {
-		this->data[keyNormalized] = { value };
+	if (element == this->m_data.end()) {
+		this->m_data[keyNormalized] = { value };
 		return;
 	}
 
@@ -54,15 +54,15 @@ void KVContainer::append(const std::string& key, const std::string value) {
 
 std::string KVContainer::get(const std::string& key) const {
 	const auto keyNormalized = Strings::toLowerCase(key);
-	const auto element = this->data.find(keyNormalized);
-	if (element == this->data.end()) return {};
+	const auto element = this->m_data.find(keyNormalized);
+	if (element == this->m_data.end()) return {};
 	return element->second.size() ? element->second.at(0) : "";
 }
 
 std::vector<std::string> KVContainer::getAll(const std::string& key) const {
 	const auto keyNormalized = Strings::toLowerCase(key);
-	const auto element = this->data.find(keyNormalized);
-	if (element == this->data.end()) return {};
+	const auto element = this->m_data.find(keyNormalized);
+	if (element == this->m_data.end()) return {};
 	return element->second;
 }
 
@@ -70,7 +70,7 @@ std::vector<KVpair> KVContainer::entries() const {
 
 	std::vector<KVpair> temp;
 
-	for (const auto& entry : this->data) {
+	for (const auto& entry : this->m_data) {
 		for (const auto& value : entry.second) {
 			temp.push_back({ entry.first, value });
 		}
@@ -80,5 +80,5 @@ std::vector<KVpair> KVContainer::entries() const {
 }
 
 size_t KVContainer::size() const noexcept {
-	return this->data.size();
+	return this->m_data.size();
 }
