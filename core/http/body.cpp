fa236d4d0c9ac37d481eec4aaa4d86560066947b
maddsua <maddsuadev@proton.me>
2024-01-24 08:03:23 +0200

    Update body.cpp

--- core/http/body.cpp
+++ core/http/body.cpp
@@ -23,23 +23,14 @@ Body::operator std::string () const {
 	return this->text();
 }
 
-/**
- * Returns body text reoresentation
-*/
 std::string Body::text() const {
 	return std::string(this->m_data.begin(), this->m_data.end());
 }
 
-/**
- * Returns raw byte buffer
-*/
 const std::vector<uint8_t>& Body::buffer() const {
 	return this->m_data;
 }
 
-/**
- * Returns body buffer size
-*/
 size_t Body::size() const {
 	return this->m_data.size();
 }
