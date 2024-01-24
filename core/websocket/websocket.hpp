00a9e89c8c80a3fdfd8f23d0272c2bd917ce77c4
maddsua <maddsuadev@proton.me>
2024-01-24 04:58:59 +0200

    Update websocket.hpp

--- core/websocket/websocket.hpp
+++ core/websocket/websocket.hpp
@@ -7,6 +7,21 @@
 
 namespace Lambda::Websocket {
 
+	enum struct CloseCode {
+		Normal = 1000,
+		GoingAway = 1001,
+		ProtocolError = 1002,
+		UnsupportedData = 1003,
+		NoStatusReceived = 1005,
+		AbnormalClose = 1006,
+		InvalidPayload = 1007,
+		PolicyViolation = 1008,
+		MessageTooBig = 1009,
+		MandatoryExtension = 1010,
+		InternalServerError = 1011,
+		TLSHandshakeFailed = 1015
+	};
+
 	class Message {
 		private:
 			std::vector<uint8_t> m_buffer;
