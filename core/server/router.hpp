98c23327ac2d938dc704eb8e4b3079f076ad93d8
maddsua <maddsuadev@proton.me>
2024-01-24 09:33:12 +0200

    Update router.hpp

--- core/server/router.hpp
+++ core/server/router.hpp
@@ -4,14 +4,22 @@
 #include "../network/tcp/connection.hpp"
 #include "../network/tcp/listen.hpp"
 #include "./server.hpp"
+#include "./handlers.hpp"
 
 #include <future>
 #include <optional>
 #include <queue>
+#include <unordered_map>
 
-namespace Lambda::Server {
+namespace Lambda {
 
-	
+	struct RouteContext {
+		Server::Handlers::HandlerFunction handler;
+	};
+
+	typedef std::unordered_map<std::string, RouteContext> Router;
+
+	std::optional<RouteContext> matchRoute(const Router& router, const std::string& pathname);
 
 };
 
