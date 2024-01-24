e9a3217d2fd76e85b67771b291fefdf6a45afe7d
maddsua <maddsuadev@proton.me>
2024-01-24 10:06:30 +0200

    Update router.hpp

--- core/server/router.hpp
+++ core/server/router.hpp
@@ -1,14 +1,9 @@
 #ifndef __LIB_MADDSUA_LAMBDA_CORE_SERVER_ROUTER__
 #define __LIB_MADDSUA_LAMBDA_CORE_SERVER_ROUTER__
 
-#include "../network/tcp/connection.hpp"
-#include "../network/tcp/listen.hpp"
-#include "./server.hpp"
 #include "./handlers.hpp"
 
-#include <future>
 #include <optional>
-#include <queue>
 #include <unordered_map>
 
 namespace Lambda {
