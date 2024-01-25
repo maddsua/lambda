8826765d7cd0c9738ea7b5a58f49c40ebf0f2291
maddsua <maddsuadev@proton.me>
2024-01-25 21:58:28 +0200

    Update Makefile.examples.mk

--- Makefile.examples.mk
+++ Makefile.examples.mk
@@ -2,7 +2,7 @@
 example.api_server: api_server.example$(EXEEXT)
 
 api_server.example$(EXEEXT): examples/api_server.o $(LAMBDA_LIBSHARED)
-	g++ -s $(CFLAGS) examples/api_server.o $(LAMBDA_LIBSHARED) $(LINK_COMPRESS_LIBS) $(LINK_SYSTEM_LIBS) -o api_server.example$(EXEEXT)
+	g++ -s $(CFLAGS) examples/api_server.o $(LAMBDA_LIBSHARED) $(LINK_OTHER_LIBS) $(LINK_SYSTEM_LIBS) -o api_server.example$(EXEEXT)
 
 examples/api_server.o: examples/api_server.cpp
 	g++ -c $(CFLAGS) examples/api_server.cpp -o examples/api_server.o
@@ -10,7 +10,7 @@ examples/api_server.o: examples/api_server.cpp
 example.kvserver: kvserver.example$(EXEEXT)
 
 kvserver.example$(EXEEXT): examples/kvserver.o $(LAMBDA_LIBSHARED)
-	g++ -s $(CFLAGS) examples/kvserver.o $(LAMBDA_LIBSHARED) $(LINK_COMPRESS_LIBS) $(LINK_SYSTEM_LIBS) -o kvserver.example$(EXEEXT)
+	g++ -s $(CFLAGS) examples/kvserver.o $(LAMBDA_LIBSHARED) $(LINK_OTHER_LIBS) $(LINK_SYSTEM_LIBS) -o kvserver.example$(EXEEXT)
 
 examples/kvserver.example.o: examples/kvserver.example.cpp
 	g++ -c $(CFLAGS) examples/kvserver.example.cpp -o examples/kvserver.example.o
