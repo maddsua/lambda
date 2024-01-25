f85e01a7d860d117366bf684e7f020e03393a6fc
maddsua <maddsuadev@proton.me>
2024-01-25 19:50:59 +0200

    Update stream.cpp

--- core/server/websocket/stream.cpp
+++ core/server/websocket/stream.cpp
@@ -10,10 +10,9 @@ using namespace Lambda;
 using namespace Lambda::Network;
 using namespace Lambda::Websocket;
 
-WebsocketStream::WebsocketStream(TCP::Connection& conn, const WebsocketInfo& infoInit) {
+WebsocketStream::WebsocketStream(TCP::Connection& tcpconn, const WebsocketInfo& infoInit) {
 
 	this->info = infoInit;
-
 
 	this->ioworker = std::async([&](){
 
@@ -31,7 +30,7 @@ WebsocketStream::~WebsocketStream() {
 }
 
 bool WebsocketStream::available() const noexcept {
-	return this->rxQueue.size();
+	return this->readQueue.size();
 }
 
 bool WebsocketStream::ok() const noexcept {
@@ -43,17 +42,18 @@ Message WebsocketStream::getMessage() {
 
 	if (!this->available()) throw std::runtime_error("no messages available");
 
-	std::lock_guard<std::mutex> lock(this->writeMutex);
+	std::lock_guard<std::mutex> lock(this->readMutex);
 
-	Message temp = this->rxQueue.front();
-	this->rxQueue.pop();
+	Message temp = this->readQueue.front();
+	this->readQueue.pop();
 	return temp;
 }
 
 void WebsocketStream::sendMessage(const Message& msg) {
 	auto messageBuffer = serializeMessage(msg);
-	std::lock_guard<std::mutex> lock(this->writeMutex);
-	this->txQueue.insert(this->txQueue.end(), messageBuffer.begin(), messageBuffer.end());
+	this->conn->write(messageBuffer);
+	//std::lock_guard<std::mutex> lock(this->writeMutex);
+	//this->txQueue.insert(this->txQueue.end(), messageBuffer.begin(), messageBuffer.end());
 }
 
 void WebsocketStream::close() {
@@ -65,21 +65,23 @@ void WebsocketStream::close(CloseCode reason) {
 	if (this->terminateFlags != StreamTerminateFlags::None) return;
 	this->terminateFlags = StreamTerminateFlags::Closed;
 
-	std::array<uint8_t, 4> frameBuff;
+	std::vector<uint8_t> frameBuff;
+	frameBuff.resize(4);
 
 	auto closeReasonByte = static_cast<std::underlying_type_t<CloseCode>>(reason);
 
 	//	control frame and close opcode
-	frameBuff[0] = 0x88;
+	frameBuff.at(0) = 0x88;
 	//	should always be 2 bytes, we only send a status code with no text
-	frameBuff[1] = sizeof(closeReasonByte);
+	frameBuff.at(1) = sizeof(closeReasonByte);
 	//	the status code itself. I hate the ppl who decided not to align ws header fields. just effing masterminds.
-	frameBuff[2] = (closeReasonByte >> 8) & 0xFF;
-	frameBuff[3] = closeReasonByte & 0xFF;
+	frameBuff.at(2) = (closeReasonByte >> 8) & 0xFF;
+	frameBuff.at(3) = closeReasonByte & 0xFF;
 
 	//	send and forget it
-	std::lock_guard<std::mutex> lock(this->writeMutex);
-	this->txQueue.insert(this->txQueue.end(), frameBuff.begin(), frameBuff.end());
+	this->conn->write(frameBuff);
+	//std::lock_guard<std::mutex> lock(this->writeMutex);
+	//this->txQueue.insert(this->txQueue.end(), frameBuff.begin(), frameBuff.end());
 
 	if (this->ioworker.valid()) {
 		this->ioworker.get();
