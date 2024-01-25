4fc5b6c641f6f7b7e4f9b6eef4a6951f42e8c8f2
maddsua <maddsuadev@proton.me>
2024-01-26 00:29:18 +0200

    Update queue.cpp

--- core/server/http/queue.cpp
+++ core/server/http/queue.cpp
@@ -14,11 +14,11 @@ static const std::map<ContentEncodings, std::string> contentEncodingMap = {
 	{ ContentEncodings::Deflate, "deflate" },
 };
 
-HttpRequestQueue::HttpRequestQueue(Network::TCP::Connection& conn, const ServeOptions& options) {
+HttpRequestQueue::HttpRequestQueue(Network::TCP::Connection& conn, const HTTPTransportOptions& options) {
 
 	this->m_reader = std::async([&](){
 		
-		const auto& conninfo = conn.getInfo();
+		const auto& conninfo = conn.info();
 		std::vector<uint8_t> recvBuff;
 		bool connectionKeepAlive = false;
 
@@ -77,7 +77,7 @@ HttpRequestQueue::HttpRequestQueue(Network::TCP::Connection& conn, const ServeOp
 				next.request.url = HTTP::URL("http://lambdahost:" + conninfo.hostPort + requestUrlString);
 			}
 
-			if (options.transport.reuseConnections) {
+			if (options.reuseConnections) {
 				auto connectionHeader = next.request.headers.get("connection");
 				if (connectionKeepAlive) connectionKeepAlive = !Strings::includes(connectionHeader, "close");
 					else connectionKeepAlive = Strings::includes(connectionHeader, "keep-alive");
@@ -180,55 +180,4 @@ bool HttpRequestQueue::await() {
 	}
 
 	return this->m_queue.size();
-}
-
-
-
-
-
-
-
-
-
-RequestQueue::RequestQueue(const std::initializer_list<RequestQueueItem>& init) {
-	this->m_queue = std::queue<RequestQueueItem>(init);
-}
-
-RequestQueue::RequestQueue(const Lambda::Server::RequestQueue& other) {
-	this->m_done = other.m_done;
-	this->m_queue = other.m_queue;
-}
-
-bool RequestQueue::hasNext() const noexcept {
-	return this->m_queue.size() > 0;
-}
-
-void RequestQueue::push(const RequestQueueItem& item) {
-	std::lock_guard<std::mutex>lock(this->m_mutex);
-	this->m_queue.push(item);
-}
-
-void RequestQueue::finish() noexcept {
-	this->m_done = true;
-}
-
-bool RequestQueue::await() {
-	while (true) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		if (this->m_queue.size() > 0) return true;
-		else if (this->m_done) return false;
-	}
-}
-
-RequestQueueItem RequestQueue::next() {
-
-	std::lock_guard<std::mutex>lock(this->m_mutex);
-
-	if (!this->hasNext())
-		throw std::runtime_error("cannot get next item from an empty PipelineQueue");
-
-	RequestQueueItem next = this->m_queue.front();
-	this->m_queue.pop();
-
-	return next;
 }
