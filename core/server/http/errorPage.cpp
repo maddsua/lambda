0a7ddecb6250a4dd3815d8c9014a5849f0d9dc39
maddsua <maddsuadev@proton.me>
2024-01-26 08:51:59 +0200

    Update errorPage.cpp

--- core/server/http/errorPage.cpp
+++ core/server/http/errorPage.cpp
@@ -7,22 +7,6 @@ using namespace Lambda;
 
 static const auto pageTemplate = std::string(&_binary_core_html_resources_servicepage_html_start, &_binary_core_html_resources_servicepage_html_end - &_binary_core_html_resources_servicepage_html_start);
 
-typedef std::vector<std::pair<std::string, std::string>> TemplateContent;
-
-std::string populateTemplate(const TemplateContent& content) {
-
-	auto result = pageTemplate;
-	auto matchIdx = std::string::npos;
-
-	for (const auto& item : content) {
-		while ((matchIdx = result.find(item.first)) != std::string::npos) {
-			result.replace(matchIdx, item.first.size(), item.second);
-		}
-	}
-
-	return result;
-}
-
 HTTP::Response HTTPServer::errorResponse(uint32_t statusCode, std::optional<std::string> errorMessage) {
 
 	auto httpstatus = Lambda::HTTP::Status(statusCode);
