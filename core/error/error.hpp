a37cd1b32aa53f5e486b89930c8678e75970f4b9
maddsua <maddsuadev@proton.me>
2024-01-26 14:39:19 +0200

    Create error.hpp

---
+++ core/error/error.hpp
@@ -0,0 +1,28 @@
+#ifndef __LIB_MADDSUA_LAMBDA_CORE_ERROR_HANDLING__
+#define __LIB_MADDSUA_LAMBDA_CORE_ERROR_HANDLING__
+
+#include <stdexcept>
+#include <string>
+
+namespace Lambda {
+
+	typedef std::exception Error;
+
+	int32_t getApiErrorCode() noexcept;
+	std::string formaErrorMessage(int32_t errorCode) noexcept;
+
+	class APIError : public Error {
+		private:
+			int32_t m_code = 0;
+			std::string m_text;
+
+		public:
+			APIError() = default;
+			const char* what() const noexcept override;
+			const std::string& message() const noexcept;
+			int32_t code() const noexcept;
+	};
+
+};
+
+#endif
