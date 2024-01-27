a1f56461773c20f2e64654d1086e95b520247705
maddsua <maddsuadev@proton.me>
2024-01-27 10:19:32 +0200

    Update apierror.cpp

--- core/error/apierror.cpp
+++ core/error/apierror.cpp
@@ -30,9 +30,7 @@ std::string Errors::formatMessage(int32_t errorCode) noexcept {
 		auto formatResult = FormatMessageA(
 			FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_MAX_WIDTH_MASK,
 			NULL, errorCode, 0, (LPSTR)&tempMessage, 0, NULL);
-		if (!formatResult) {
-			return "Windows API error " + std::to_string(errorCode);
-		}
+		if (!formatResult) return "Windows API error " + std::to_string(errorCode);
 
 		auto message = std::string(tempMessage);
 		LocalFree(tempMessage);
@@ -49,7 +47,7 @@ std::string Errors::formatMessage(int32_t errorCode) noexcept {
 		#else
 
 			char tempBuff[128];
-			auto messagePtr = strerror_r(errorCode, tempBuff, sizeof(tempBuff));
+			char* messagePtr = strerror_r(errorCode, tempBuff, sizeof(tempBuff));
 			if (messagePtr != nullptr) return messagePtr;
 			return "OS error " + std::to_string(errorCode);
 
