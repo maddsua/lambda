eece4135c0d364eb0ce7271431f6fe25119ac8c1
maddsua <maddsuadev@proton.me>
2024-01-27 09:23:58 +0200

    Update error.hpp

--- core/error/error.hpp
+++ core/error/error.hpp
@@ -9,11 +9,11 @@ namespace Lambda {
 	typedef std::exception Error;
 
 	namespace Errors {
-		int32_t getApiErrorCode() noexcept;
-		std::string formatErrorMessage(int32_t errorCode) noexcept;
+		int32_t getApiError() noexcept;
+		std::string formatMessage(int32_t errorCode) noexcept;
 	};
 
-	class APIError : public Error {
+	/*class APIError : public Error {
 		private:
 			int32_t m_code = 0;
 			std::string m_text;
@@ -26,7 +26,7 @@ namespace Lambda {
 			const char* what() const noexcept override;
 			const std::string& message() const noexcept;
 			int32_t code() const noexcept;
-	};
+	};*/
 
 };
 
