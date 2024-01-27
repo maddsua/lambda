ae86b40466a1a20770b509e89672a729a8b03f42
maddsua <maddsuadev@proton.me>
2024-01-28 01:43:08 +0200

    Update generateinclude.cpp

--- tools/generateinclude.cpp
+++ tools/generateinclude.cpp
@@ -4,19 +4,12 @@
 #include <stdexcept>
 
 #include "../version.hpp"
+#include "../core/polyfill/polyfill.hpp"
 
 struct Options {
 	std::string inputHeader;
 	std::string outputHeader;
 };
-
-std::string loadFile(const std::string& path) {
-	std::string content;
-	auto localfile = std::ifstream(path);
-	if (!localfile.is_open()) throw std::runtime_error("Couldn't open file " + path + " for read");
-	content.assign(std::istreambuf_iterator<char>(localfile), std::istreambuf_iterator<char>());
-	return content;
-}
 
 int main(int argc, char const *argv[]) {
 
@@ -33,22 +26,58 @@ int main(int argc, char const *argv[]) {
 		auto key = arg.substr(2, separatorPos - 2);
 		auto value = arg.substr(separatorPos + 1);
 
-		if (key == "template") opts.inputHeader = value;
+		if (key == "entrypoint") opts.inputHeader = value;
 		else if (key == "output") opts.outputHeader = value;
 	}
 
 	try {
 
 		if (!opts.inputHeader.size())
-			throw std::runtime_error("ABORTED: Provide template file with --template=[filelocation]");
+			throw std::runtime_error("ABORTED: Provide entrypoint file with --entrypoint=[filelocation]");
 
 		if (!opts.outputHeader.size())
 			throw std::runtime_error("ABORTED: Provide output file with --output=[filelocation]");
 
 	} catch(const std::exception& e) {
 		std::cerr << e.what() << '\n';
+		return 1;
 	}
 
+	std::ifstream file(opts.inputHeader);
+
+	if (!file.is_open()) {
+		std::cerr << "ahhh shit could'nt read the file \"" << opts.inputHeader << "\"\n";
+		return 1;
+	}
+
+	std::vector<std::string> lines;
+	std::string line;
+	while (std::getline(file, line))
+		lines.push_back(line);
+
+	file.close();
+
+	std::vector<std::pair<std::string, std::string>> replacementPairs;
+
+	for (const auto& line : lines) {
+
+		if (!Lambda::Strings::trim(line).starts_with("#include")) continue;
+
+		auto pathStart = line.find('\"');
+		if (pathStart == std::string::npos)
+			throw std::runtime_error("invalid include statement path start");
+
+		auto pathEnd = line.find('\"', pathStart);
+		if (pathEnd == std::string::npos)
+			throw std::runtime_error("invalid include statement path end");
+
+		auto filePath = line.substr(pathStart, pathEnd - pathStart);
+		replacementPairs.push_back({ line, filePath });
+	}
+
+	for (const auto& item : replacementPairs) {
+		puts(item.second.c_str());
+	}
 
 	return 0;
 }
