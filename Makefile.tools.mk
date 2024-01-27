1d489e77fb2de56960ce6bd941170b19d6f96a87
maddsua <maddsuadev@proton.me>
2024-01-28 01:20:03 +0200

    Update Makefile.tools.mk

--- Makefile.tools.mk
+++ Makefile.tools.mk
@@ -3,3 +3,9 @@ updatedllinfo: updatedllinfo$(EXEEXT)
 
 updatedllinfo$(EXEEXT): tools/updatedllinfo.cpp
 	g++ -std=c++20 -s tools/updatedllinfo.cpp -o updatedllinfo$(EXEEXT)
+
+
+generateinclude: generateinclude$(EXEEXT)
+
+generateinclude$(EXEEXT): tools/generateinclude.cpp
+	g++ -std=c++20 -s tools/generateinclude.cpp -o generateinclude$(EXEEXT)
