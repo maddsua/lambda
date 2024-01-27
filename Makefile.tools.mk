ac10a529a629a592f9f403b91f040354e72f5051
maddsua <maddsuadev@proton.me>
2024-01-28 01:23:46 +0200

    Update Makefile.tools.mk

--- Makefile.tools.mk
+++ Makefile.tools.mk
@@ -1,11 +1,13 @@
 
-updatedllinfo: updatedllinfo$(EXEEXT)
+TOOL_UPDATEDLLINFO_TARGET = $(TEMPBIN)updatedllinfo$(EXEEXT)
+tool.updatedllinfo: $(TOOL_UPDATEDLLINFO_TARGET)
 
-updatedllinfo$(EXEEXT): tools/updatedllinfo.cpp
-	g++ -std=c++20 -s tools/updatedllinfo.cpp -o updatedllinfo$(EXEEXT)
+$(TOOL_UPDATEDLLINFO_TARGET): tools/updatedllinfo.cpp
+	g++ -std=c++20 -s tools/updatedllinfo.cpp -o $(TOOL_UPDATEDLLINFO_TARGET)
 
 
-generateinclude: generateinclude$(EXEEXT)
+TOOL_GENERATEINCLUDE_TARGET = $(TEMPBIN)generateinclude$(EXEEXT)
+tool.generateinclude: $(TOOL_GENERATEINCLUDE_TARGET)
 
-generateinclude$(EXEEXT): tools/generateinclude.cpp
-	g++ -std=c++20 -s tools/generateinclude.cpp -o generateinclude$(EXEEXT)
+$(TOOL_GENERATEINCLUDE_TARGET): tools/generateinclude.cpp
+	g++ -std=c++20 -s tools/generateinclude.cpp -o $(TOOL_GENERATEINCLUDE_TARGET)
