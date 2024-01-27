ed24474803860e9109224983923978dd024c957e
maddsua <maddsuadev@proton.me>
2024-01-27 08:28:21 +0200

    Update apierror.cpp

--- core/error/apierror.cpp
+++ core/error/apierror.cpp
@@ -25,38 +25,35 @@ int32_t ErrorHandling::getApiErrorCode() noexcept {
 
 std::string ErrorHandling::formatErrorMessage(int32_t errorCode) noexcept {
 
-	std::string message;
-
 	#ifdef _WIN32
 
 		char* tempMessage = nullptr;
 		auto formatResult = FormatMessageA(
 			FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
 			NULL, errorCode, 0, (LPSTR)&tempMessage, 0, NULL);
-		if (formatResult) {
-			message = tempMessage;
-			LocalFree(tempMessage);
-		} else {
-			message = "Windows API error " + std::to_string(errorCode);
+		if (!formatResult) {
+			return "Windows API error " + std::to_string(errorCode);
 		}
+
+		auto message = std::string(tempMessage);
+		LocalFree(tempMessage);
+		return message;
 
 	#else
 
 		#ifdef sys_errlist
-			message = (errorCode < sys_nerr) ? 
+			return (errorCode < sys_nerr) ? 
 				sys_errlist[errorCode] :
 				("OS error " + std::to_string(errorCode));
 		#else
 			char tempBuff[128];
 			tempBuff[sizeof(tempBuff) - 1] = 0;
 			if (strerror_s(tempBuff, sizeof(tempBuff) - 1, errorCode)) {
-				message = "OS error " + std::to_string(errorCode);
-			} else message = tempBuff;
+				return "OS error " + std::to_string(errorCode);
+			} else return tempBuff;
 		#endif
 
 	#endif
-
-	return message;
 }
 
 APIError::APIError() {
