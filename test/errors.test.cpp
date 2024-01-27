1b665ec9f1718c2e0b125db37343855ada54e4b1
maddsua <maddsuadev@proton.me>
2024-01-27 08:44:04 +0200

    Create errors.test.cpp

---
+++ test/errors.test.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include "../core/error/error.hpp"
+
+int main(int argc, char const *argv[]) {
+
+	for (size_t i = 0; i < 25; i++) {
+		auto errorMessage = Lambda::Errors::formatErrorMessage(i);
+		printf("Test error message: %s\n", errorMessage.c_str());
+	}
+	
+	return 0;
+}
