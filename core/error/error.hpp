0d538fa0c67ed260006e09ce13d8389d68a72b07
maddsua <maddsuadev@proton.me>
2024-01-27 09:25:29 +0200

    Update error.hpp

--- core/error/error.hpp
+++ core/error/error.hpp
@@ -4,30 +4,9 @@
 #include <stdexcept>
 #include <string>
 
-namespace Lambda {
-
-	typedef std::exception Error;
-
-	namespace Errors {
-		int32_t getApiError() noexcept;
-		std::string formatMessage(int32_t errorCode) noexcept;
-	};
-
-	/*class APIError : public Error {
-		private:
-			int32_t m_code = 0;
-			std::string m_text;
-
-		public:
-			APIError();
-			APIError(int32_t code);
-			APIError(const std::string& commentText);
-			APIError(const APIError& other);
-			const char* what() const noexcept override;
-			const std::string& message() const noexcept;
-			int32_t code() const noexcept;
-	};*/
-
+namespace Lambda::Errors {
+	int32_t getApiError() noexcept;
+	std::string formatMessage(int32_t errorCode) noexcept;
 };
 
 #endif
