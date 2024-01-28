3c05485bc5352f2f77e9d73c96fdf67c8345e706
maddsua <maddsuadev@proton.me>
2024-01-28 21:28:04 +0200

    Update version.hpp

--- version.hpp
+++ version.hpp
@@ -1,11 +1,3 @@
-/**
-	An http and websocket C++ library
-	Or at least, it apperas to be
-	2023 maddsua, https://github.com/maddsua
-	
-	This file is not the entrypoint.
-	Include lambda.hpp instead
-*/
 
 #ifndef __LIB_MADDSUA_LAMBDA_VERSION__
 #define __LIB_MADDSUA_LAMBDA_VERSION__
