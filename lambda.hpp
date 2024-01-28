10143cc92bdf41550b57d797111d3dc5c4f5e410
maddsua <maddsuadev@proton.me>
2024-01-28 20:43:31 +0200

    Update lambda.hpp

--- lambda.hpp
+++ lambda.hpp
@@ -9,26 +9,26 @@
 #ifndef __LIB_MADDSUA_LAMBDA__
 #define __LIB_MADDSUA_LAMBDA__
 
-#include "./version.hpp"
+	#include "./version.hpp"
 
-//	include core "modules"
-#include "./core/http/http.hpp"
-#include "./core/server/server.hpp"
-#include "./core/polyfill/polyfill.hpp"
-#include "./core/json/json.hpp"
-#include "./core/encoding/encoding.hpp"
-#include "./core/crypto/crypto.hpp"
-#include "./core/compression/compression.hpp"
-#include "./core/error/error.hpp"
-#include "./core/html/templates.hpp"
+	//	include core "modules"
+	#include "./core/http/http.hpp"
+	#include "./core/server/server.hpp"
+	#include "./core/polyfill/polyfill.hpp"
+	#include "./core/json/json.hpp"
+	#include "./core/encoding/encoding.hpp"
+	#include "./core/crypto/crypto.hpp"
+	#include "./core/compression/compression.hpp"
+	#include "./core/error/error.hpp"
+	#include "./core/html/templates.hpp"
 
-//	include extra "modules"
-#include "./extra/storage/storage.hpp"
+	//	include extra "modules"
+	#include "./extra/storage/storage.hpp"
 
-namespace Lambda {
-	typedef HTTP::Request Request;
-	typedef HTTP::Response Response;
-	typedef RequestContext Context;
-};
+	namespace Lambda {
+		typedef HTTP::Request Request;
+		typedef HTTP::Response Response;
+		typedef RequestContext Context;
+	};
 
 #endif
