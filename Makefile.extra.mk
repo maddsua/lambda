ac8e2ca7df9e77c4d7afb5c6d1a242f4dc3feee4
maddsua <maddsuadev@proton.me>
2024-01-28 22:49:07 +0200

    Update Makefile.extra.mk

--- Makefile.extra.mk
+++ Makefile.extra.mk
@@ -2,7 +2,7 @@ LIB_EXTRA				=	extra/extra.a
 LIB_EXTRA_DEPS			=	$(LIB_EXTRA_STORAGE_DEPS)
 
 LIB_EXTRA_STORAGE		=	extra/storage.a
-LIB_EXTRA_STORAGE_DEPS	=	extra/webstorage/localStorage.o extra/webstorage/sessionStorage.o
+LIB_EXTRA_STORAGE_DEPS	=	extra/webstorage/driver.o extra/webstorage/interface.o
 
 
 # extra object
@@ -16,8 +16,8 @@ $(LIB_EXTRA): $(LIB_EXTRA_DEPS)
 $(LIB_EXTRA_STORAGE): $(LIB_EXTRA_STORAGE_DEPS)
 	ar rvs $(LIB_EXTRA_STORAGE) $(LIB_EXTRA_STORAGE_DEPS)
 
-extra/webstorage/localStorage.o: extra/webstorage/localStorage.cpp
-	g++ -c $(CFLAGS) extra/webstorage/localStorage.cpp -o extra/webstorage/localStorage.o
+extra/webstorage/driver.o: extra/webstorage/driver.cpp
+	g++ -c $(CFLAGS) extra/webstorage/driver.cpp -o extra/webstorage/driver.o
 
-extra/webstorage/sessionStorage.o: extra/webstorage/sessionStorage.cpp
-	g++ -c $(CFLAGS) extra/webstorage/sessionStorage.cpp -o extra/webstorage/sessionStorage.o
+extra/webstorage/interface.o: extra/webstorage/interface.cpp
+	g++ -c $(CFLAGS) extra/webstorage/interface.cpp -o extra/webstorage/interface.o
