a50ac3f492d8d5be09287efe6a6384b92f3161c5
maddsua <maddsuadev@proton.me>
2024-01-29 04:38:33 +0200

    Update storage.test.cpp

--- test/storage.test.cpp
+++ test/storage.test.cpp
@@ -7,9 +7,40 @@ using namespace Lambda::Storage;
 
 int main(int argc, char const *argv[]) {
 
-	LocalStorage localStorage("test/data/storage.db");
+	auto testData = std::initializer_list<std::pair<std::string, std::string>>({
+		{ "sample record", "sample value" },
+		{ "test_prop_1", "quick brown horse... whatever" },
+		{ "test_prop_2", "very long string lol no it's not" }
+	});
 
-	std::cout << "Get from local 'test_prop_1': " << localStorage.getItem("test_prop_1") << "\n";
+
+	//	put some stuff into storage
+	{
+		LocalStorage localStorage("test/data/storage.db");
+
+		for (const auto& item : testData) {
+			localStorage.setItem(item.first, item.second);
+		}
+
+		puts("Write ok");
+	}
+
+	//	now get it out of there
+	{
+		LocalStorage localStorage("test/data/storage.db");
+
+		for (const auto& item : testData) {
+			auto value = localStorage.getItem(item.first);
+			if (value != item.second) {
+				throw std::runtime_error("data didn't match for record \"" + item.first + "\"\n\nExpected:\n" + item.second + "\n\nGot:\n" + value);
+			}
+		}
+
+		puts("Data ok");
+	}
+
+
+	/*std::cout << "Get from local 'test_prop_1': " << localStorage.getItem("test_prop_1") << "\n";
 	std::cout << "Get from local 'not_to_remove': " << localStorage.getItem("not_to_remove") << "\n";
 
 	localStorage.setItem("test_prop_1", "quick brown horse... whatever");
@@ -20,7 +51,7 @@ int main(int argc, char const *argv[]) {
 	localStorage.setItem("not_to_remove", "this record will not be removed");
 
 	localStorage.removeItem("to_remove");
-	localStorage.removeItem("undefined");
+	localStorage.removeItem("undefined");*/
 
 	return 0;
 }
