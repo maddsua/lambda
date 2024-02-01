479355bb3135b17855b4ee3010ec10f8f290d811
maddsua <maddsuadev@proton.me>
2024-02-01 02:59:42 +0200

    Update http.hpp

--- core/server/http.hpp
+++ core/server/http.hpp
@@ -24,6 +24,7 @@ namespace Lambda::HTTPServer {
 
 	struct IncomingRequest {
 		HTTP::Request request;
+		std::string pathname;
 		ContentEncodings acceptsEncoding = ContentEncodings::None;
 		bool keepAlive = false;
 	};
