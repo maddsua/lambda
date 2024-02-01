519d1842d2c872ec27ae31366311429a85956262
maddsua <maddsuadev@proton.me>
2024-02-01 04:21:12 +0200

    Update connection.cpp

--- core/server/handlers/connection.cpp
+++ core/server/handlers/connection.cpp
@@ -1,5 +1,6 @@
 #include "../server.hpp"
 #include "../handlers.hpp"
+#include "../../polyfill/polyfill.hpp"
 #include "../http.hpp"
 
 using namespace Lambda;
@@ -8,8 +9,36 @@ using namespace Lambda::HTTPServer;
 using namespace Lambda::Server::Handlers;
 
 void Handlers::connectionHandler(Network::TCP::Connection&& conn, const ServeOptions& config, const ConnectionCallback& handlerCallback) noexcept {
-	auto connctx = IncomingConnection(&conn, config.transport);
-	handlerCallback(connctx);
+
+	auto createLogTimeStamp = [&]() {
+		if (config.loglevel.timestamps) {
+			return Date().toHRTString() + ' ';
+		}
+		return std::string();
+	};
+
+	try {
+
+		auto connctx = IncomingConnection(&conn, config.transport);
+		handlerCallback(connctx);
+
+	} catch(const std::exception& e) {
+
+		if (config.loglevel.requests) fprintf(stderr,
+			"%s[Service] Connection to %s terminated: %s\n",
+			createLogTimeStamp().c_str(),
+			conn.info().remoteAddr.hostname.c_str(),
+			e.what()
+		);
+
+	} catch(...) {
+
+		if (config.loglevel.requests) fprintf(stderr,
+			"%s[Service] Connection to %s terminated (unknown error)\n",
+			createLogTimeStamp().c_str(),
+			conn.info().remoteAddr.hostname.c_str()
+		);
+	}
 }
 
 IncomingConnection::IncomingConnection(Network::TCP::Connection* conn, const HTTPTransportOptions& opts) {
