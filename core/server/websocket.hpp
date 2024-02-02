f88e9e5ca12284567e5ea8eb7b6d757e8a24dbce
maddsua <maddsuadev@proton.me>
2024-02-02 14:37:49 +0200

    Update websocket.hpp

--- core/server/websocket.hpp
+++ core/server/websocket.hpp
@@ -2,19 +2,35 @@
 #define __LIB_MADDSUA_LAMBDA_CORE_WEBSOCKET_SERVER__
 
 #include <functional>
-#include <optional>
 #include <future>
+#include <queue>
+#include <mutex>
 
 #include "../network/network.hpp"
 #include "../websocket/websocket.hpp"
 
+namespace Lambda::HTTPServer {
+	class ConnectionContext;
+};
+
 namespace Lambda::WSServer {
 
-	struct WebsocketContext {
-		bool awaitMessage() const noexcept;
-		Websocket::Message nextMessage();
-		void sendMessage(const Websocket::Message& msg);
-		void close(Websocket::CloseReason reason);
+	class WebsocketContext {
+		private:
+			HTTPServer::ConnectionContext* connctx = nullptr;
+			std::future<void> m_reader;
+			std::queue<Websocket::Message> m_queue;
+			std::mutex m_read_lock;
+
+		public:
+			WebsocketContext(HTTPServer::ConnectionContext* connctxInit);
+			~WebsocketContext();
+
+			bool awaitMessage();
+			bool hasMessage() const noexcept;
+			Websocket::Message nextMessage();
+			void sendMessage(const Websocket::Message& msg);
+			void close(Websocket::CloseReason reason);
 	};
 
 };
