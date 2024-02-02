ea65d8af646d9b2e9e5717b2124af7f33e9f698c
maddsua <maddsuadev@proton.me>
2024-02-02 13:50:40 +0200

    Create websocket.hpp

---
+++ core/websocket/websocket.hpp
@@ -0,0 +1,28 @@
+#ifndef __LIB_MADDSUA_LAMBDA_CORE_WEBSOCKETS__
+#define __LIB_MADDSUA_LAMBDA_CORE_WEBSOCKETS__
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstdint>
+#include <mutex>
+#include <future>
+
+namespace Lambda::Websocket {
+
+	struct Message {
+		const std::vector<uint8_t> data;
+		const bool binary = false;
+		const bool partial = false;
+		const time_t timestamp = 0;
+
+		Message(const std::string& dataInit);
+		Message(const std::string& dataInit, bool partial);
+		Message(const std::vector<uint8_t>& dataInit);
+		Message(const std::vector<uint8_t>& dataInit, bool partial);
+		std::string text() const noexcept;
+		size_t size() const noexcept;
+	};
+};
+
+#endif
