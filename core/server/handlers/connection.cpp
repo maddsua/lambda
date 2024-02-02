e36d3d7d0dea71d2495bdc6efe2b24e1543ab0e2
maddsua <maddsuadev@proton.me>
2024-02-02 17:09:18 +0200

    Update connection.cpp

--- core/server/handlers/connection.cpp
+++ core/server/handlers/connection.cpp
@@ -31,7 +31,7 @@ void Handlers::connectionHandler(
 
 	try {
 
-		auto connctx = IncomingConnection(&conn, config.transport);
+		auto connctx = IncomingConnection(conn, config.transport);
 		handlerCallback(connctx);
 
 	} catch(const std::exception& e) {
