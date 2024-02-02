2518569562835e864271e0173dec2bedfb196309
maddsua <maddsuadev@proton.me>
2024-02-02 14:37:51 +0200

    Create context.cpp

---
+++ core/server/ws/context.cpp
@@ -0,0 +1,17 @@
+#include "../websocket.hpp"
+
+using namespace Lambda;
+using namespace Lambda::Websocket;
+using namespace Lambda::WSServer;
+
+WebsocketContext::WebsocketContext(HTTPServer::ConnectionContext* connctxInit) {
+	if (!connctxInit) throw std::runtime_error("ConnectionContext* cannot be a nullptr");
+	this->connctx = connctxInit;
+}
+
+WebsocketContext::~WebsocketContext() {
+	if (this->m_reader.valid()) {
+		try { this->m_reader.get(); }
+			catch (...) {}
+	}
+}
