1925a03be91cb8c42575412ccf8976af319c4cc6
maddsua <maddsuadev@proton.me>
2024-02-02 14:37:53 +0200

    Create queue.cpp

---
+++ core/server/ws/queue.cpp
@@ -0,0 +1,41 @@
+#include "../websocket.hpp"
+
+using namespace Lambda;
+using namespace Lambda::Websocket;
+using namespace Lambda::WSServer;
+
+bool WebsocketContext::hasMessage() const noexcept {
+	return this->m_queue.size() > 0;
+}
+
+Message WebsocketContext::nextMessage() {
+
+	if (!this->m_queue.size()) {
+		throw std::runtime_error("cannot get next item from an empty HttpRequestQueue");
+	}
+
+	std::lock_guard<std::mutex>lock(this->m_read_lock);
+
+	Message temp = this->m_queue.front();
+	this->m_queue.pop();
+
+	return temp;
+}
+
+bool WebsocketContext::awaitMessage() {
+
+	if (!this->m_reader.valid()) {
+		return this->m_queue.size();
+	}
+
+	auto readerDone = false;
+	while (!readerDone && !this->m_queue.size()) {
+		readerDone = this->m_reader.wait_for(std::chrono::milliseconds(1)) == std::future_status::ready;
+	}
+
+	if (readerDone) {
+		this->m_reader.get();
+	}
+
+	return this->m_queue.size();
+}
