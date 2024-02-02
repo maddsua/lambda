33f58ffb8ec957aaf42b120fa606096b9bca8a71
maddsua <maddsuadev@proton.me>
2024-02-03 00:11:29 +0200

    Update websocket.deno.ts

--- test/clients/websocket.deno.ts
+++ test/clients/websocket.deno.ts
@@ -4,3 +4,11 @@ const exampleSocket = new WebSocket("ws://localhost:8180/");
 exampleSocket.onopen = () => {
 	exampleSocket.send("Yo hi there!");
 };
+
+exampleSocket.onmessage = (msg) => {
+	console.log(msg.data);
+}
+
+exampleSocket.onerror = (err) => {
+	console.log((err as ErrorEvent).message);
+}
