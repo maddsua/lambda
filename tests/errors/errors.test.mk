7d83c936827b51563342ee25f3789634cdc179ca
maddsua <maddsuadev@proton.me>
2024-02-04 01:12:44 +0200

    Update errors.test.mk

--- tests/errors/errors.test.mk
+++ tests/errors/errors.test.mk
@@ -4,8 +4,8 @@ TEST_ERRORS_TARGET = $(EXEPFX)errors.test$(EXEEXT)
 test.errors: $(TEST_ERRORS_TARGET)
 	$(TEST_ERRORS_TARGET)
 
-$(TEST_ERRORS_TARGET): tests/errors/main.o $(LIB_CORE_ERROR_DEPS)
-	g++ $(CFLAGS) tests/errors/main.cpp $(LIB_CORE_ERROR_DEPS) -o $(TEST_ERRORS_TARGET)
+$(TEST_ERRORS_TARGET): tests/errors/main.o $(LIB_CORE_UTILS_DEPS)
+	g++ $(CFLAGS) tests/errors/main.cpp $(LIB_CORE_UTILS_DEPS) -o $(TEST_ERRORS_TARGET)
 
 tests/errors/main.o: tests/errors/main.cpp
 	g++ -c $(CFLAGS) tests/errors/main.cpp -o tests/errors/main.o
