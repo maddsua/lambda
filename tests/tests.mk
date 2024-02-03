f9866beacdb24db42c141252630907040ebe7c8c
maddsua <maddsuadev@proton.me>
2024-02-04 00:16:05 +0200

    done?

--- tests/tests.mk
+++ tests/tests.mk
@@ -1,10 +1,10 @@
 
-include tests/zlib/Makefile
-include tests/brotli/Makefile
-include tests/storage/Makefile
-include tests/encoding/Makefile
-include tests/url/Makefile
-include tests/json/Makefile
-include tests/strings/Makefile
-include tests/errors/Makefile
-include tests/tcp/Makefile
+include tests/zlib/zlib.test.mk
+include tests/brotli/brotli.test.mk
+include tests/storage/storage.test.mk
+include tests/encoding/encoding.test.mk
+include tests/url/url.test.mk
+include tests/json/json.test.mk
+include tests/strings/strings.test.mk
+include tests/errors/errors.test.mk
+include tests/tcp/tcp.test.mk
