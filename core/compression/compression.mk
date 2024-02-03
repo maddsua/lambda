d4d7b30ec26dd459cb6604e73c047d115d4ffccc
maddsua <maddsuadev@proton.me>
2024-02-04 00:59:02 +0200

    Update compression.mk

--- core/compression/compression.mk
+++ core/compression/compression.mk
@@ -1,13 +1,10 @@
 
 LIB_CORE_COMPRESS		=	core/compression.a
-LIB_CORE_COMPRESS_DEPS	=	core/compression/streams.o core/compression/brotli.o core/compression/zlib.o
+LIB_CORE_COMPRESS_DEPS	=	core/compression/brotli.o core/compression/zlib.o
 
 # compression stuff
 $(LIB_CORE_COMPRESS): $(LIB_CORE_COMPRESS_DEPS)
 	ar rvs $(LIB_CORE_COMPRESS) $(LIB_CORE_COMPRESS_DEPS)
-
-core/compression/streams.o: core/compression/streams.cpp
-	g++ -c $(CFLAGS) core/compression/streams.cpp -o core/compression/streams.o
 
 core/compression/brotli.o: core/compression/brotli.cpp
 	g++ -c $(CFLAGS) core/compression/brotli.cpp -o core/compression/brotli.o
