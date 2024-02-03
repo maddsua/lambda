65495548b3ef0433b8b263f526810b2211d67d69
maddsua <maddsuadev@proton.me>
2024-02-04 01:03:11 +0200

    Update utils.hpp

--- core/utils/utils.hpp
+++ core/utils/utils.hpp
@@ -66,15 +66,15 @@ namespace Lambda {
 
 	namespace Literals {
 
-		constexpr size_t operator""_GB(size_t const size) {
+		constexpr size_t operator""_GB(size_t size) {
 			return 1024 * 1024 * 1024 * size;
 		}
 
-		constexpr size_t operator""_MB(size_t const size) {
+		constexpr size_t operator""_MB(size_t size) {
 			return 1024 * 1024 * size;
 		}
 
-		constexpr size_t operator""_KB(size_t const size) {
+		constexpr size_t operator""_KB(size_t size) {
 			return 1024 * size;
 		}
 	};
