2c00f707197d231552761d9af21d68ff5a858ded
maddsua <maddsuadev@proton.me>
2024-02-04 00:13:17 +0200

    now it's better

--- core/core.mk
+++ core/core.mk
@@ -1,16 +1,16 @@
 
-include core/compression/Makefile
-include core/crypto/Makefile
-include core/encoding/Makefile
-include core/error/Makefile
-include core/html/Makefile
-include core/http/Makefile
-include core/json/Makefile
-include core/network/Makefile
-include core/polyfill/Makefile
-include core/server/Makefile
-include core/utils/Makefile
-include core/websocket/Makefile
+include core/compression/compression.mk
+include core/crypto/crypto.mk
+include core/encoding/encoding.mk
+include core/error/error.mk
+include core/html/html.mk
+include core/http/http.mk
+include core/json/json.mk
+include core/network/network.mk
+include core/polyfill/polyfill.mk
+include core/server/server.mk
+include core/utils/utils.mk
+include core/websocket/websocket.mk
 
 LIB_CORE				= core/core.a
 LIB_CORE_DEPS			= $(LIB_CORE_POLYFILL_DEPS) $(LIB_CORE_HTTP_DEPS) $(LIB_CORE_ENCODING_DEPS) $(LIB_CORE_NETWORK_DEPS) $(LIB_CORE_COMPRESS_DEPS) $(LIB_CORE_SERVER_DEPS) $(LIB_CORE_CRYPTO_DEPS) $(LIB_CORE_HTML_DEPS) $(LIB_CORE_JSON_DEPS) $(LIB_CORE_ERROR_DEPS) $(LIB_CORE_WEBSOCKET_DEPS) $(LIB_CORE_UTILS_DEPS)
