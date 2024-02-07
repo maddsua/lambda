48ffe8ee3f1a22f536ec7cc9f40353e512b6f9d2
maddsua <maddsuadev@proton.me>
2024-02-07 23:01:47 +0200

    Update formats.hpp

--- extra/vfs/formats.hpp
+++ extra/vfs/formats.hpp
@@ -3,13 +3,33 @@
 
 #include "./vfs.hpp"
 
+#include <queue>
+
 namespace Lambda::VFS::Formats {
+
+	class FSQueue {
+		private:
+			std::queue<VirtualFile> m_queue;
+			std::mutex m_lock;
+			bool m_done = false;
+
+		public:
+			FSQueue& operator=(const FSQueue& other) = delete;
+			FSQueue& operator=(FSQueue&& other) noexcept;
+
+			bool await();
+			VirtualFile next();
+			void push(VirtualFile&& item);
+			void close() noexcept;
+			bool done() const noexcept;
+	};
 
 	namespace Tar {
 
 		extern const std::initializer_list<std::string> supportedExtensions;
 
 	};
+	
 };
 
 #endif
