49a15498bd37546cc52fa6946b973014a505351e
maddsua <maddsuadev@proton.me>
2024-02-07 22:50:59 +0200

    Update format_tar.cpp

--- extra/vfs/format_tar.cpp
+++ extra/vfs/format_tar.cpp
@@ -2,5 +2,9 @@
 
 using namespace Lambda;
 using namespace Lambda::VFS;
-using namespace Lambda::VFS::Tar;
+using namespace Lambda::VFS::Formats;
+using namespace Lambda::VFS::Formats::Tar;
 
+const std::initializer_list<std::string> Tar::supportedExtensions {
+	".tar", ".tar.gz", ".tgz"
+};
