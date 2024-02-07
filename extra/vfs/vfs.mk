ef626dc13a1e62e92da3451c240105346aa2de9a
maddsua <maddsuadev@proton.me>
2024-02-07 23:02:22 +0200

    Update vfs.mk

--- extra/vfs/vfs.mk
+++ extra/vfs/vfs.mk
@@ -1,6 +1,6 @@
 
 LIB_EXTRA_VFS			=	extra/vfs.a
-LIB_EXTRA_VFS_DEPS		=	extra/vfs/interface.o
+LIB_EXTRA_VFS_DEPS		=	extra/vfs/interface.o extra/vfs/fsqueue.o extra/vfs/format_tar.o
 
 # storage stuff
 $(LIB_EXTRA_VFS): $(LIB_EXTRA_VFS_DEPS)
@@ -8,3 +8,9 @@ $(LIB_EXTRA_VFS): $(LIB_EXTRA_VFS_DEPS)
 
 extra/vfs/interface.o: extra/vfs/interface.cpp
 	g++ -c $(CFLAGS) extra/vfs/interface.cpp -o extra/vfs/interface.o
+
+extra/vfs/fsqueue.o: extra/vfs/fsqueue.cpp
+	g++ -c $(CFLAGS) extra/vfs/fsqueue.cpp -o extra/vfs/fsqueue.o
+
+extra/vfs/format_tar.o: extra/vfs/format_tar.cpp
+	g++ -c $(CFLAGS) extra/vfs/format_tar.cpp -o extra/vfs/format_tar.o
