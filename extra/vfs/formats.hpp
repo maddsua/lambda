0a1ffc8862ce1524dc60f1f6177466c5ed783799
maddsua <maddsuadev@proton.me>
2024-02-07 22:33:31 +0200

    Create formats.hpp

---
+++ extra/vfs/formats.hpp
@@ -0,0 +1,13 @@
+#ifndef __LIB_MADDSUA_LAMBDA_EXTRA_VFS_FORMATS__
+#define __LIB_MADDSUA_LAMBDA_EXTRA_VFS_FORMATS__
+
+#include "./vfs.hpp"
+
+namespace Lambda::VFS {
+
+	namespace Tar {
+
+	};
+};
+
+#endif
