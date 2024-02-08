a83a6a2c349b89d1f7e3e9ae38225dbcbbf802e1
maddsua <maddsuadev@proton.me>
2024-02-08 05:33:45 +0200

    Update vfs.test.mk

--- tests/vfs/vfs.test.mk
+++ tests/vfs/vfs.test.mk
@@ -1,10 +1,10 @@
 
 # Test vfs extra module
 TEST_VFS_TARGET = $(EXEPFX)vfs.test$(EXEEXT)
-TEST_VFS_DEPS	= $(LIB_CORE_POLYFILL_DEPS) $(LIB_EXTRA_VFS_DEPS)
+TEST_VFS_DEPS	= $(LIB_CORE_POLYFILL_DEPS) $(LIB_EXTRA_VFS_DEPS) $(LIB_CORE_COMPRESS_DEPS)
 
 test.vfs: $(TEST_VFS_TARGET)
 	$(TEST_VFS_TARGET)
 
 $(TEST_VFS_TARGET): tests/vfs/main.cpp $(TEST_VFS_DEPS)
-	g++ $(CFLAGS) tests/vfs/main.cpp $(TEST_VFS_DEPS) -o $(TEST_VFS_TARGET)
+	g++ $(CFLAGS) tests/vfs/main.cpp $(TEST_VFS_DEPS) -lz -o $(TEST_VFS_TARGET)
