7320aa6df99c3194982afb2b9f7c6af5d6735931
maddsua <maddsuadev@proton.me>
2024-02-11 22:05:21 +0200

    Update sse.hpp

--- core/sse/sse.hpp
+++ core/sse/sse.hpp
@@ -2,6 +2,7 @@
 #define __LIB_MADDSUA_LAMBDA_CORE_SERVER_SIDE_EVENTS__
 
 #include "../network/tcp/connection.hpp"
+#include "../http/transport.hpp"
 
 #include <string>
 #include <map>
@@ -21,7 +22,7 @@ namespace Lambda::SSE {
 			Network::TCP::Connection& m_conn;
 
 		public:
-			Writer(Network::TCP::Connection& conn);
+			Writer(HTTP::Transport::V1TransportContext& httpCtx);
 			void push(const EventMessage& event);
 	};
 };
