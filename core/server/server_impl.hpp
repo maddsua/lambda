0d4fba320665f1548f9663f9f7be80b4b170eda0
maddsua <maddsuadev@proton.me>
2024-02-12 14:52:11 +0200

    Update server_impl.hpp

--- core/server/server_impl.hpp
+++ core/server/server_impl.hpp
@@ -18,6 +18,20 @@ namespace Lambda::Server {
 		HTTP::Response renderErrorPage(HTTP::Status code, const std::string& message, ErrorResponseType type);
 		HTTP::Response renderErrorPage(HTTP::Status code, const std::string& message);
 	};
+
+	namespace Connections {
+
+		class UpgradeError : public Lambda::Error {
+			private:
+				const HTTP::Status m_status = 400;
+
+			public:
+				UpgradeError(const std::string& message) : Error(message) {}
+				UpgradeError(const std::string& message, HTTP::Status status) : Error(message), m_status(status) {}
+
+				const HTTP::Status& status() const noexcept { return this->m_status; }
+		};
+	};
 };
 
 #endif
