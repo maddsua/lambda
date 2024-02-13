392612627528e001668e058ca06440793b984fb1
maddsua <maddsuadev@proton.me>
2024-02-13 21:53:31 +0200

    Update method.cpp

--- core/http/method.cpp
+++ core/http/method.cpp
@@ -46,7 +46,7 @@ void Method::apply(const std::string& method) {
 	auto knownMethod = httpKnownMethods.find(Strings::toUpperCase(Strings::trim(method)));
 
 	if (knownMethod == httpKnownMethods.end()) {
-		throw std::invalid_argument("provided http method is unknown");
+		throw std::invalid_argument("HTTP method \"" + method + "\" is unknown");
 	}
 
 	this->value = knownMethod->second;
@@ -60,11 +60,15 @@ Method::operator Methods () const noexcept {
 	return this->value;
 }
 
+bool Method::operator==(const Method& other) const noexcept {
+	return this->value == other.value;
+}
+
 bool Method::operator==(const std::string& other) const noexcept {
-	return this->toString() == other;
+	return this->toString() == Strings::toUpperCase(other);
 }
 
 bool Method::operator==(const char* other) const noexcept {
 	if (!other) return false;
-	return this->toString() == other;
+	return this->toString() == Strings::toUpperCase(other);
 }
