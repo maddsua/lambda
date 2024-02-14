a21b93534e170b41dbaffb7a4bc5afdedb1d4081
maddsua <maddsuadev@proton.me>
2024-02-14 06:32:33 +0200

    Update main.cpp

--- examples/handler_crash/main.cpp
+++ examples/handler_crash/main.cpp
@@ -8,12 +8,12 @@ using namespace Lambda::JSON;
 int main(int argc, char const *argv[]) {
 
 	auto handler = [&](const Request& req, const Context& context) {
-		throw std::runtime_error("test error");
+		throw std::runtime_error("Test error. It's all fine!");
 		return HTTP::Response();
 	};
 
 	ServerConfig initparams;
-	initparams.errorResponseType = ErrorResponseType::JSON;
+	initparams.errorResponseType = ErrorResponseType::HTML;
 	initparams.loglevel.requests = true;
 	auto server = LambdaInstance(handler, initparams);
 	server.awaitFinished();
