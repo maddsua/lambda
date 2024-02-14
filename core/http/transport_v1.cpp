9c5cfad657f078ba94866de895b77ef6e99d1fa2
maddsua <maddsuadev@proton.me>
2024-02-14 09:32:50 +0200

    Update transport_v1.cpp

--- core/http/transport_v1.cpp
+++ core/http/transport_v1.cpp
@@ -300,7 +300,7 @@ void TransportContextV1::respond(const ResponseContext& responsectx) {
 	}
 
 	std::vector<uint8_t> headerBuff;
-	headerBuff.reserve(2048);
+	headerBuff.reserve(TCP::Connection::DefaultChunkSize);
 
 	//	push http version
 	static const std::string httpVersion = "HTTP/1.1";
@@ -388,7 +388,7 @@ void TransportContextV1::writeRaw(const std::vector<uint8_t>& data) {
 }
 
 std::vector<uint8_t> TransportContextV1::readRaw() {
-	return this->readRaw(Network::TCP::Connection::ReadChunkSize);
+	return this->readRaw(Network::TCP::Connection::DefaultChunkSize);
 }
 
 std::vector<uint8_t> TransportContextV1::readRaw(size_t expectedSize) {
