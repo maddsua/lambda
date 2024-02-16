b250165b6d875959416f8d9c290ca7c80024cba7
maddsua <maddsuadev@proton.me>
2024-02-16 05:09:23 +0200

    Create buildopts.hpp

---
+++ buildopts.hpp
@@ -0,0 +1,11 @@
+
+#ifndef __LIB_MADDSUA_LAMBDA_BUILD_OPTIONS__
+#define __LIB_MADDSUA_LAMBDA_BUILD_OPTIONS__
+
+	/**
+	 * You can comment this line if you don't any compression libs to be used.
+	 * This disables both zlib and brotli
+	*/
+	//#define LAMBDA_BUILDOPTS_ENABLE_COMPRESSION
+
+#endif
